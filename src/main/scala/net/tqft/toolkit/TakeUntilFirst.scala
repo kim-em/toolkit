0556000f6be4f72f3e8e7d8cd8821fd35178e1ad
Scott Morrison <scott@tqft.net>
2011-09-03 15:22:48 -0700

    TakeUntilFirst

---
+++ src/main/scala/net/tqft/toolkit/TakeUntilFirst.scala
@@ -0,0 +1,23 @@
+package net.tqft.toolkit
+
+object TakeUntilFirst {
+  implicit def takeUntilFirst[A](x: Iterable[A]) = new TakeUntilFirstable(x)
+  class TakeUntilFirstable[A](x: Iterable[A]) {
+    def takeUntilFirst(condition: A => Boolean): Iterable[A] = {
+      new Iterable[A] {
+        def iterator = new Iterator[A] {
+          val inner = x.iterator
+          var found = false
+          def hasNext = !found && inner.hasNext
+          def next = {
+            val result = inner.next
+            if (condition(result)) {
+              found = true
+            }
+            result
+          }
+        }
+      }
+    }
+  }
+}
