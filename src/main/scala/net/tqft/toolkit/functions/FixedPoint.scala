eb54ff7c3aca455cfa52efd2e85d64c13258abde
Scott Morrison <scott@tqft.net>
2012-04-18 15:45:26 -0700

    almost there, at least for 2-by-2s

--- src/main/scala/net/tqft/toolkit/functions/FixedPoint.scala
+++ src/main/scala/net/tqft/toolkit/functions/FixedPoint.scala
@@ -1,16 +1,25 @@
 package net.tqft.toolkit.functions
 
+import net.tqft.toolkit.collections.NonStrictNaturalNumbers
+
 object FixedPoint {
 
   implicit def fixedPointForFunction[A](f: A => A) = new FunctionFixedPoint(f)
 
-  class FunctionFixedPoint[A](f: A => A) {
+  class FunctionFixedPoint[A](f: A => A, equalityTest: (A, A) => Boolean = { (p: A, q: A) => p == q }) {
+    def this(f: A => A, compareBy: A => Any) = this(f, { (p: A, q: A) => compareBy(p) == compareBy(q) })
+    
     def fixedPoint(a: A): A = {
       val iterates = Stream.iterate(a)(f)
-      ((iterates zip iterates.tail) find { case (a, b) => a == b }).get._1
+      ((iterates zip iterates.tail) find { case (a, b) => equalityTest(a, b) }).get._1
+    }
+    def firstRepeatedIteration(a: A) = {
+      val iterates = Stream.iterate(a)(f)
+      iterates(NonStrictNaturalNumbers.find({ n => iterates.take(n).toList.find({b:A => equalityTest(iterates(n), b)}).nonEmpty}).get)
     }
   }
 
   def apply[A](f: A => A): A => A = f.fixedPoint(_)
+  def comparingBy[A, B](c: A => B)(f: A => A): A => A = new FunctionFixedPoint(f, c).fixedPoint(_)
 
 }
