4e36aeeba3429e07158ae3c672872694b0e15aa7
Scott Morrison <scott@tqft.net>
2012-04-22 00:46:10 -0700

    doesn't even look too bad. still impossible, of course

--- src/main/scala/org/fusionatlas/eigenvalues/SharpenEigenvalues.scala
+++ src/main/scala/org/fusionatlas/eigenvalues/SharpenEigenvalues.scala
@@ -8,13 +8,17 @@ import net.tqft.toolkit.algebra.Implicits
 import net.tqft.toolkit.functions.FixedPoint
 import net.tqft.toolkit.algebra.Gadgets
 
+  import java.math.{ BigDecimal => jBigDecimal } 
+
+
 object SharpenEigenvalues extends App {
   case class EigenvalueEstimate(approximateEigenvalue: BigDecimal, approximateEigenvector: List[BigDecimal])
-  
-  def sharpenEigenvalueOneStep(matrix: Matrix[BigDecimal], bounds: (BigDecimal, BigDecimal))(estimate: EigenvalueEstimate): EigenvalueEstimate = {
+
+  def sharpenEigenvalueOneStep(matrix: Matrix[Byte], bounds: (BigDecimal, BigDecimal))(estimate: EigenvalueEstimate): EigenvalueEstimate = {
     require(matrix.numberOfColumns == matrix.numberOfRows)
-    implicit val bigDecimals  = Gadgets.BigDecimals(estimate.approximateEigenvalue.mc)
-    
+    val mc = estimate.approximateEigenvalue.mc
+    implicit val bigDecimals = Gadgets.BigDecimals(mc)
+
     val matrices = Matrices.matricesOver(bigDecimals, matrix.numberOfRows)
 
     def sqrt(x: BigDecimal): BigDecimal = {
@@ -32,43 +36,53 @@ object SharpenEigenvalues extends App {
 
     val checkedEstimate = bounds._1.min(bounds._2.max(estimate.approximateEigenvalue))
 
-    val mm = matrices.subtract(matrix, matrices.scalarMultiply(checkedEstimate, matrices.identityMorphism()))
+    val mm = Matrix(matrix.entries.zipWithIndex.map {
+      case (row: scala.collection.mutable.WrappedArray[_], index) => {
+        val decimalRow = row.map(x => BigDecimal(x.toInt, mc))
+        decimalRow(index) = decimalRow(index) - checkedEstimate
+        decimalRow
+      } 
+    })
     val aa = mm.preimageOf(estimate.approximateEigenvector).getOrElse(estimate.approximateEigenvector)
     val improvedEigenvector = normalize(aa)
-    val newEstimate = norm(matrix(improvedEigenvector))
+    val action = for(row <- matrix.entries) yield {
+      (improvedEigenvector zip row).map(p => p._1 * p._2).sum
+    }
+    val newEstimate = norm(action)
     println(newEstimate)
     EigenvalueEstimate(newEstimate, improvedEigenvector)
   }
 
-  def sharpenEigenvalue(matrix: Matrix[BigDecimal], bounds: (BigDecimal, BigDecimal), precision: Int)(estimate: EigenvalueEstimate): EigenvalueEstimate = {
+  def sharpenEigenvalue(matrix: Matrix[Byte], bounds: (BigDecimal, BigDecimal), precision: Int)(estimate: EigenvalueEstimate): EigenvalueEstimate = {
     import FixedPoint._
     val field = Gadgets.BigDecimals(precision)
-    val fixedPoint = FixedPoint.sameTest({ (p:EigenvalueEstimate,q:EigenvalueEstimate) => field.chop(p.approximateEigenvalue - q.approximateEigenvalue, field.epsilon * 1000) == field.zero}) _
+    val fixedPoint = FixedPoint.sameTest({ (p: EigenvalueEstimate, q: EigenvalueEstimate) => field.chop(p.approximateEigenvalue - q.approximateEigenvalue, field.epsilon * 1000) == field.zero }) _
     fixedPoint(sharpenEigenvalueOneStep(matrix, bounds) _)(estimate)
-//    (sharpenEigenvalueOneStep(matrix, bounds) _).firstRepeatedIteration(estimate)
   }
 
   def findEigenvalueAndEigenvector(matrix: Matrix[Byte], bounds: (Double, Double), precision: Int): EigenvalueEstimate = {
     val (mc, initialEstimate) =
       if (precision < 34) {
         val mc = java.math.MathContext.DECIMAL128
-//        val mc = new java.math.MathContext(100, java.math.RoundingMode.HALF_EVEN)
-        (mc, EigenvalueEstimate(BigDecimal((bounds._1 + bounds._2) / 2, mc), List.fill(matrix.numberOfColumns)(BigDecimal(scala.util.Random.nextDouble(), mc))))
+        //        val mc = new java.math.MathContext(100, java.math.RoundingMode.HALF_EVEN)
+        (mc, EigenvalueEstimate(BigDecimal(bounds._1 + (bounds._2 - bounds._1) * scala.util.Random.nextDouble, mc), List.fill(matrix.numberOfColumns)(BigDecimal(scala.util.Random.nextDouble(), mc))))
       } else {
         val mc = new java.math.MathContext(precision, java.math.RoundingMode.HALF_EVEN)
-        val EigenvalueEstimate(previousEstimate, previousEigenvector) = findEigenvalueAndEigenvector(matrix, bounds, 1* precision / 2)
+        val EigenvalueEstimate(previousEstimate, previousEigenvector) = findEigenvalueAndEigenvector(matrix, bounds, 1 * precision / 2)
         (mc, EigenvalueEstimate(previousEstimate(mc), previousEigenvector map { x => x(mc) }))
       }
     val sharpBounds = (BigDecimal(bounds._1, mc), BigDecimal(bounds._2, mc))
-    val decimalMatrix = Matrix(matrix.entries.map { row => row.map { x => BigDecimal(x.toInt, mc) } })
-    sharpenEigenvalue(decimalMatrix, sharpBounds, precision)(initialEstimate)
+    sharpenEigenvalue(matrix, sharpBounds, precision)(initialEstimate)
   }
-  
+
   def findEigenvalue(matrix: Matrix[Byte], bounds: (Double, Double), precision: Int) = findEigenvalueAndEigenvector(matrix, bounds, precision).approximateEigenvalue
 
   val matrix = Matrix(List(List(7, 1), List(2, 5)).map(_.map(_.toByte)))
   val bounds = (4.1, 4.3)
-  
-  println(findEigenvalue(Hadamard12.transfer2, (19.9, 20.2), 100))
-//  println(findEigenvalue(matrix, bounds, 100))
+
+  while (true) {
+//    println(findEigenvalue(Hadamard12.transfer2, (19.9, 20.2), 100))
+    println(findEigenvalue(Hadamard12.transfer3, (59.9, 60.1), 100))
+  }
+  //  println(findEigenvalue(matrix, bounds, 100))
 }
