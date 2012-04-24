70aee315939514714f9350e3fb3beb0a2f3925f1
Scott Morrison <scott@tqft.net>
2012-04-24 01:41:38 -0700

    trying to run on the command line...

---
+++ src/main/scala/org/fusionatlas/eigenvalues/SharpenEigenvaluesHadoop.scala
@@ -0,0 +1,14 @@
+package org.fusionatlas.eigenvalues
+
+import net.tqft.toolkit.hadoop.HadoopSeq
+import net.tqft.toolkit.algebra.Matrix
+
+object SharpenEigenvaluesHadoop extends App {
+  val (target, bounds) = args(0) match {
+    case "2" => (Hadamard12.transfer2, (19.9, 20.2))
+    case "3" => (Hadamard12.transfer3, (59.9, 60.1))
+    case "4" => (Hadamard12.transfer4, (353.95, 352.97))
+  }
+    val hadoopTarget = new Matrix(target.numberOfColumns, HadoopSeq.from(target.entries))
+    println(SharpenEigenvalues.findEigenvalue(hadoopTarget, bounds, 100))
+}
