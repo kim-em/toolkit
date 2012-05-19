704303ce7965088885c8693bc741d4f2c0ce0cb7
Scott Morrison <scott@tqft.net>
2012-05-18 23:30:38 -0700

    still doesn't work in 2.10.0-M3

--- src/main/scala/LazyValDeadlock.scala
+++ src/main/scala/LazyValDeadlock.scala
@@ -1,6 +1,8 @@
 object LazyValDeadLock extends App {
   lazy val X = 0
   lazy val Y = {
+    // 'mentioning' X here solves the problem:
+    // X
     for (i <- 0 until 2 par) yield  {
       println(i)
       X
