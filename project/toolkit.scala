50f289ffcee3840f727c5188429e1c7cfcb5ffcb
Scott Morrison <scott@tqft.net>
2012-06-02 17:04:28 -0500

    updating build file, dependencies are almost certainly not all there

--- project/toolkit.scala
+++ project/toolkit.scala
@@ -2,14 +2,25 @@ import sbt._
 
 object Toolkit extends Build {
     lazy val root = Project(id = "toolkit",
-                            base = file(".")) dependsOn(functions) aggregate()
+                            base = file(".")) aggregate(base, arithmetic, amazon, collections, algebra, functions)
+
+    lazy val base = Project(id = "toolkit.base",
+                           base = file("base")) dependsOn()
 
     lazy val arithmetic = Project(id = "toolkit.arithmetic",
                            base = file("arithmetic")) dependsOn()
 
+    lazy val amazon = Project(id = "toolkit.amazon",
+                           base = file("amazon")) dependsOn()
+
+    lazy val collections = Project(id = "toolkit.collections",
+                           base = file("collections")) dependsOn(functions)
+
     lazy val algebra = Project(id = "toolkit.algebra",
-                           base = file("algebra")) dependsOn(root, arithmetic)
+                           base = file("algebra")) dependsOn(base, arithmetic)
 
     lazy val functions = Project(id = "toolkit.functions",
                            base = file("functions"))
+
+
 }
