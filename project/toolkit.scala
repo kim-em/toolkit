e0887547a1f73fc67ca98777b988e7772e0b003e
Scott Morrison <scott@tqft.net>
2012-06-08 12:00:17 -0700

    removing all the build.sbt files

--- project/toolkit.scala
+++ project/toolkit.scala
@@ -2,69 +2,119 @@ import sbt._
 import Keys._
 
 object Toolkit extends Build {
-    import BuildSettings._
+  import BuildSettings._
+  import Dependencies._
 
-    lazy val root = Project(id = "toolkit",
-                            base = file("."),
- 			    settings = buildSettings
-	) aggregate(base, arithmetic, amazon, collections, algebra, functions, eval)
+  lazy val root = Project(id = "toolkit",
+    base = file("."),
+    settings = buildSettings) aggregate (base, arithmetic, amazon, collections, algebra, functions, eval)
 
-    lazy val base: Project = Project(id = "toolkit.base",
-                           base = file("base"),
-			   settings = buildSettings
-	) dependsOn()
+  lazy val base: Project = Project(id = "toolkit.base",
+    base = file("base"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= Seq(commons.logging))) dependsOn ()
 
-    lazy val arithmetic = Project(id = "toolkit.arithmetic",
-                           base = file("arithmetic"),
-			   settings = buildSettings
-	) dependsOn()
+  lazy val arithmetic = Project(id = "toolkit.arithmetic",
+    base = file("arithmetic"),
+    settings = buildSettings) dependsOn ()
 
-    lazy val amazon = Project(id = "toolkit.amazon",
-                           base = file("amazon"),
-			   settings = buildSettings
-	) dependsOn(base,collections)
+  lazy val amazon = Project(id = "toolkit.amazon",
+    base = file("amazon"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= Seq(jets3t, typica))) dependsOn (base, collections)
 
-    lazy val collections = Project(id = "toolkit.collections",
-                           base = file("collections"),
-			   settings = buildSettings
-	) dependsOn(base, functions)
+  lazy val collections = Project(id = "toolkit.collections",
+    base = file("collections"),
+    settings = buildSettings) dependsOn (base, functions)
 
-    lazy val algebra = Project(id = "toolkit.algebra",
-                           base = file("algebra"),
-			   settings = buildSettings
-	) dependsOn(base, arithmetic, functions, collections)
+  lazy val algebra = Project(id = "toolkit.algebra",
+    base = file("algebra"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= Seq(commons.math, apfloat))) dependsOn (base, arithmetic, functions, collections)
 
-    lazy val functions = Project(id = "toolkit.functions",
-                           base = file("functions"),
-                           settings = buildSettings
-        )
+  lazy val functions = Project(id = "toolkit.functions",
+    base = file("functions"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= Seq(guava)))
 
-    lazy val eval = Project(id = "toolkit.eval",
-                           base = file("eval"),
-                           settings = buildSettings
-        )
+  lazy val eval = Project(id = "toolkit.eval",
+    base = file("eval"),
+    settings = buildSettings ++ Seq(libraryDependencies ++= Seq(compiler(buildScalaVersion))))
 
 }
 
 object BuildSettings {
   import Resolvers._
+  import Dependencies._
 
   val buildOrganization = "net.tqft"
-  val buildVersion      = "0.1.6"
+  val buildVersion = "0.1.6"
   val buildScalaVersion = "2.10.0-M3"
 
-  val buildSettings = Defaults.defaultSettings ++ Seq (
+  val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
-    version      := buildVersion,
+    version := buildVersion,
     scalaVersion := buildScalaVersion,
-    publishTo    := Some(Resolver.sftp("toolkit.tqft.net Maven repository", "tqft.net", "tqft.net/releases") as ("scottmorrison", new java.io.File("/Users/scott/.ssh/id_rsa"))),
-    resolvers    := sonatypeResolvers
-  )
+    publishTo := Some(Resolver.sftp("toolkit.tqft.net Maven repository", "tqft.net", "tqft.net/releases") as ("scottmorrison", new java.io.File("/Users/scott/.ssh/id_rsa"))),
+    resolvers := sonatypeResolvers /* ++ SonatypeSettings.publishing */,
+    libraryDependencies += {
+        val (scalatestVersion, scalatestScalaVersion) = buildScalaVersion match {
+                case sv if sv.startsWith("2.9") => ("1.8", "2.9.0")
+                case sv if sv.startsWith("2.10") => ("1.8-SNAPSHOT", "2.10.0-M3")
+        }
+        ("org.scalatest" % ("scalatest_" + scalatestScalaVersion) % scalatestVersion % "test" )
+    },
+    libraryDependencies ++= Seq(junit, slf4j))
+}
+
+object SonatypeSettings {
+/*
+  val publishing = Seq(
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { x => false },
+    pomExtra := (
+      <url>http://bitbucket.org/scottmorrison/toolkit</url>
+      <licenses>
+        <license>
+          <name>CC-BY</name>
+          <url>http://creativecommons.org/licenses/by/3.0/</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>https://bitbucket.org/scottmorrison/toolkit</url>
+      </scm>
+      <developers>
+        <developer>
+          <id>scott</id>
+          <name>Scott Morrison</name>
+          <url>http://tqft.net</url>
+        </developer>
+      </developers>),
+    publishTo <<= version { v: String =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    })
+    */
 }
 
 object Resolvers {
-	val sonatypeResolvers = Seq(
-	        "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-	        "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
-	)
+  val sonatypeResolvers = Seq(
+    "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+    "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases")
 }
+
+object Dependencies {
+        val junit = "junit" % "junit" % "4.8" % "test"
+        val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.6.1"
+	val apfloat = "org.apfloat" % "apfloat" % "1.6.3"		// arbitrary precision integers and floats; much better than BigInt and BigDecimal
+	object commons {
+		val math = "org.apache.commons" % "commons-math" % "2.2"	// simplex algorithm
+		val logging = "commons-logging" % "commons-logging" % "1.1.1"
+	}
+        val jets3t = "net.java.dev.jets3t" % "jets3t" % "0.8.1"
+        val typica = "com.google.code.typica" % "typica" % "1.7.2"
+	val guava = "com.google.guava" % "guava" % "12.0"
+        def compiler(scalaVersion: String) = "org.scala-lang" % "scala-compiler" % scalaVersion
+}
+
