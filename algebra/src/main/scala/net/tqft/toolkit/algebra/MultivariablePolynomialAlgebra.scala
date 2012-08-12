58f1b5314bd7e16b671b5ac783e7549802a073bb
Scott Morrison <scott@tqft.net>
2012-08-12 13:41:33 +1000

    cleanup of multivariable polynomials, and starting a solver over the naturals

---
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/MultivariablePolynomialAlgebra.scala
@@ -0,0 +1,21 @@
+package net.tqft.toolkit.algebra
+
+trait MultivariablePolynomialAlgebra[A, V] extends FreeModuleOnMonoid[A, Map[V, Int], MultivariablePolynomial[A, V]] with AssociativeAlgebra[A, MultivariablePolynomial[A, V]] {
+  override def wrap(terms: List[(Map[V, Int], A)]): MultivariablePolynomial[A, V] = MultivariablePolynomialImpl(terms)
+  private case class MultivariablePolynomialImpl(terms: List[(Map[V, Int], A)]) extends MultivariablePolynomial[A, V]
+
+  override object monoid extends CommutativeMonoid[Map[V, Int]] {
+    override val zero = Map.empty[V, Int]
+    override def add(x: Map[V, Int], y: Map[V, Int]): Map[V, Int] = {
+      (for (k <- x.keySet ++ y.keySet) yield {
+        k -> Gadgets.Integers.add(x.getOrElse(k, 0), y.getOrElse(k, 0))
+      }).toMap
+    }
+  }
+}
+
+object MultivariablePolynomialAlgebras {
+  def over[A: Ring, V] = new MultivariablePolynomialAlgebra[A, V] {
+    override val ring = implicitly[Ring[A]]
+  }
+}
