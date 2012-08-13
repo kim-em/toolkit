dc657f3ca6333eddf9a39201fa22fef4c8d2c25a
Scott Morrison <scott@tqft.net>
2012-08-13 20:44:17 +1000

    another go at the solver

--- algebra/src/main/scala/net/tqft/toolkit/algebra/IntegerPolynomialProgramming.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/IntegerPolynomialProgramming.scala
@@ -5,57 +5,37 @@ object IntegerPolynomialProgramming {
   // not exactly integer polynomial programming;
   // we try to find positive integer roots of the polynomials
   def solve[V](polynomials: Set[MultivariablePolynomial[Int, V]], variables: Set[V]): Iterable[Map[V, Int]] = {
-    trait Solution {
-      def remainingPolynomials: Set[MultivariablePolynomial[Int, V]]
-      def values: Map[V, Int]
-    }
-
-    trait Solver {
-      def solutions: Iterable[Solution]
-      def consider(p: MultivariablePolynomial[Int, V]): Solver = {
-        p.terms match {
-          case (m, x) :: Nil => {
-            // just one term
-            m.keySet.toSeq match {
-              case Seq() => {
-                // a non-zero constant
-                require(x != 0)
-                Impossible
-              }
-              case Seq(v) => {
-                m(v) match {
-                  case 1 => {
-                    // v must be zero!
-                    ???
-                  }
-                }
-              }
-              case _ => {
-                // a monomial with several factors
-                ???
-              }
-            }
-          }
-          case _ => ???
-        }
+    
+    val polynomialAlgebra = MultivariablePolynomialAlgebras.over[Int, V](Gadgets.Integers)
+    
+    trait SolveStrategy {
+      def solutions: Map[V, Int]
+      def substitutions: Map[V, MultivariablePolynomial[Int, V]]
+      def polynomials: Set[MultivariablePolynomial[Int, V]]
+      def consider(p: MultivariablePolynomial[Int, V]): Iterable[SolveStrategy] 
+      
+      protected def simplify(p: MultivariablePolynomial[Int, V]): MultivariablePolynomial[Int, V] = {
+        polynomialAlgebra.substitute(solutions.mapValues(polynomialAlgebra.constant(_)) ++ substitutions)(p)
       }
     }
-
-    case object Impossible extends Solver {
-      override def solutions = Iterable.empty
-      override def consider(p: MultivariablePolynomial[Int, V]) = this
+    
+    case class TrivialSolver(solutions: Map[V, Int], substitutions: Map[V, MultivariablePolynomial[Int, V]], polynomials: Set[MultivariablePolynomial[Int, V]]) extends SolveStrategy {
+      override def consider(p: MultivariablePolynomial[Int, V]): Iterable[SolveStrategy] = Iterable(copy(polynomials = polynomials + p))
     }
-
-    case object EmptySolution extends Solution {
-      override def remainingPolynomials = polynomials
-      override def values = Map.empty
+    
+    case class EasySolver(solutions: Map[V, Int], substitutions: Map[V, MultivariablePolynomial[Int, V]], polynomials: Set[MultivariablePolynomial[Int, V]]) extends SolveStrategy {
+      override def consider(p: MultivariablePolynomial[Int, V]): Iterable[EasySolver] = {
+        ???
+      }
     }
-
-    case object EmptySolver extends Solver {
-      override def solutions = Iterable(EmptySolution)
-    }
-
-    polynomials.foldLeft[Solver](EmptySolver)(_.consider(_)).solutions.map(_.values)
+    
+    val strategies: List[(Map[V,Int], Map[V, MultivariablePolynomial[Int, V]], Set[MultivariablePolynomial[Int, V]]) => SolveStrategy] = List(TrivialSolver, EasySolver)
+    
+    val empty: SolveStrategy = strategies.head(Map.empty, Map.empty, Set.empty)
+    
+    val finished = polynomials.foldLeft(Iterable(empty))((i, p) => i.flatMap(_.consider(p)))
+    for(s <- finished; if s.polynomials.isEmpty) yield s.solutions
+      
   }
 
 }
