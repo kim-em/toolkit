8464409f7c424046b15776f6f2f447eda35e2967
Scott Morrison <scott@tqft.net>
2012-08-22 13:49:18 +1000

    Adding admissibility constraints

--- algebra/src/main/scala/net/tqft/toolkit/algebra/FusionBimodule.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/FusionBimodule.scala
@@ -20,50 +20,65 @@ trait FusionBimodule[A] extends FiniteDimensionalFreeModule[A] {
       })
   }
   def identityConstraints = leftRing.identityConstraints ++ rightRing.identityConstraints ++ leftModule.identityConstraints ++ rightModule.identityConstraints
-  
+  def admissibilityConstraints = {
+    leftModule.admissibilityConstraints ++ rightModule.admissibilityConstraints ++ (
+      for (m <- leftModule.basis.iterator; n <- rightModule.basis) yield {
+        Seq(
+          coefficients.subtract(
+            leftRing.innerProduct(leftModule.rightMultiplicationByDuals(m, n), leftModule.rightMultiplicationByDuals(m, n)),
+            rightRing.innerProduct(rightModule.rightMultiplicationByDuals(m, m), rightModule.rightMultiplicationByDuals(n, n))),
+          coefficients.subtract(
+            rightRing.innerProduct(rightModule.rightMultiplicationByDuals(n, m), rightModule.rightMultiplicationByDuals(n, m)),
+            leftRing.innerProduct(leftModule.rightMultiplicationByDuals(m, m), leftModule.rightMultiplicationByDuals(n, n))))
+      }).flatten
+  }
+
   def verifyAssociativity = {
-    associativityConstraints.find(_ != zero).isEmpty
+    associativityConstraints.forall(_ == zero)
+  }
+  def verifyAdmissibility = {
+    admissibilityConstraints.forall(_ == coefficients.zero)
   }
   def verifyIdentity = {
-    identityConstraints.find(_ != zero).isEmpty
-  }  
+    identityConstraints.forall(_ == zero)
+  }
 
 }
 
 trait FusionBimoduleWithLeftDimensions extends FusionBimodule[Int] {
   override val leftRing: FusionRingWithDimensions
   override val rightRing: ConcreteFusionRing
-  override  def leftModule: leftRing.FusionModule
+  override def leftModule: leftRing.FusionModule
   override def rightModule: rightRing.FusionModule
 
   def verifyRightSmallerThanLeftInequalities = {
-    (for(b <- leftModule.basis.iterator) yield {
+    (for (b <- leftModule.basis.iterator) yield {
       val lb = rightModule.dimensionLowerBounds(b)
       val ub = leftModule.dimensionUpperBounds(b)
       lb < ub
     }).forall(_ == true)
   }
-  
+
   def rightRingDimensionLowerBounds(x: Seq[Int]): Double = {
     // for each simple y in the bimodule category, compute yx (another object in the bimodule category)
     // d(x) = d(yx)/d(y)
-    
-//    println("lower bounds for " + x)
-    val lowerBound = (for(y <- leftModule.basis) yield {
+
+    //    println("lower bounds for " + x)
+    val lowerBound = (for (y <- leftModule.basis) yield {
       val dxy = leftModule.dimensionLowerBounds(rightModule.act(x, y))
-      val dy =  leftModule.dimensionUpperBounds(y)
-//      println("lower bound from " + y + ": " + dxy / dy + " = " + dxy + "/"+dy)
+      val dy = leftModule.dimensionUpperBounds(y)
+      //      println("lower bound from " + y + ": " + dxy / dy + " = " + dxy + "/"+dy)
       dxy / dy
     }).max
-//    println("lower bounds from the ring: " + rightRing.dimensionLowerBounds(x))
-    
+    //    println("lower bounds from the ring: " + rightRing.dimensionLowerBounds(x))
+
     List(lowerBound, rightRing.dimensionLowerBounds(x)).max
   }
-  
+
   def verifyGlobalDimensionInequality = {
-    val leftGlobalDimensionUpperBound = (for(b <- leftRing.basis) yield leftRing.dimensionUpperBounds(b) * leftRing.dimensionUpperBounds(b)).sum
-    val rightGlobalDimensionLowerBound = (for(b <- rightRing.basis) yield rightRingDimensionLowerBounds(b) * rightRingDimensionLowerBounds(b)).sum
-//    print("[ub: " + leftGlobalDimensionUpperBound+ ", lb: " + rightGlobalDimensionLowerBound + "]")
+    val leftGlobalDimensionUpperBound = (for (b <- leftRing.basis) yield leftRing.dimensionUpperBounds(b) * leftRing.dimensionUpperBounds(b)).sum
+    val rightGlobalDimensionLowerBound = (for (b <- rightRing.basis) yield rightRingDimensionLowerBounds(b) * rightRingDimensionLowerBounds(b)).sum
+    //    print("[ub: " + leftGlobalDimensionUpperBound+ ", lb: " + rightGlobalDimensionLowerBound + "]")
     rightGlobalDimensionLowerBound < leftGlobalDimensionUpperBound
   }
 }
@@ -73,9 +88,9 @@ trait FusionBimoduleWithDimensions extends FusionBimoduleWithLeftDimensions {
   override def rightModule: rightRing.FusionModule
 
   def verifyLeftSmallerThanRightInequalities = {
-    (for(b <- leftModule.basis.iterator) yield leftModule.dimensionLowerBounds(b) < rightModule.dimensionUpperBounds(b)).forall(_ == true)
+    (for (b <- leftModule.basis.iterator) yield leftModule.dimensionLowerBounds(b) < rightModule.dimensionUpperBounds(b)).forall(_ == true)
   }
-  def verifyInequalities = verifyLeftSmallerThanRightInequalities && verifyRightSmallerThanLeftInequalities  
+  def verifyInequalities = verifyLeftSmallerThanRightInequalities && verifyRightSmallerThanLeftInequalities
 }
 
 object FusionBimodule {
@@ -98,8 +113,7 @@ object FusionBimoduleWithLeftDimensions {
     override val rightRing = FusionRing(rightMultiplication)
 
     override val leftModule = leftRing.moduleFromStructureCoefficients(_leftModule.structureCoefficients)
-    override val rightModule = rightRing.moduleFromStructureCoefficients(rightAction)    
+    override val rightModule = rightRing.moduleFromStructureCoefficients(rightAction)
   }
 
-  
 }
--- algebra/src/main/scala/net/tqft/toolkit/algebra/FusionBimodules.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/FusionBimodules.scala
@@ -57,7 +57,7 @@ object FusionBimodules {
      bimodule.verifyRightSmallerThanLeftInequalities && bimodule.verifyGlobalDimensionInequality
     }
 
-    val polynomials = (variableBimodule.associativityConstraints.flatten ++ variableBimodule.identityConstraints.flatten ++ variableBimodule.rightRing.dualityConstraints(otherDuality).flatten).toSeq
+    val polynomials = (variableBimodule.associativityConstraints.flatten ++ variableBimodule.admissibilityConstraints ++ variableBimodule.identityConstraints.flatten ++ variableBimodule.rightRing.dualityConstraints(otherDuality).flatten).toSeq
     val variables = (fusionModuleUnknowns.flatMap(_.entries).flatten.flatMap(_.variables.toSeq) ++ fusionRingUnknowns.flatMap(_.entries).flatten.flatMap(_.variables.toSeq)).distinct
     require(variables.size == otherRank * otherRank * otherRank + leftModule.rank * otherRank * leftModule.rank)
     
