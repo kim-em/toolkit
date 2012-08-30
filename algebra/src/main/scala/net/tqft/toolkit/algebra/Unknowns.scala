38616acaa6b8d8ca6de57222c9287c931d653a5e
Scott Morrison <scott@tqft.net>
2012-08-30 14:49:28 +0200

    cleanup

--- algebra/src/main/scala/net/tqft/toolkit/algebra/Unknowns.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/Unknowns.scala
@@ -23,16 +23,4 @@ object Unknowns {
   
   def apply[A](ring: Ring[A]): Ring[Option[A]] = new RingWithUnknowns[A](ring)
   
-  def apply[A](fusionRing: FusionRing[A]): FusionRing[Option[A]] = new FusionRing[Option[A]] {
-    val rank = fusionRing.rank
-    def coefficients = Unknowns(fusionRing.coefficients)
-    override def multiply(x: Seq[Option[A]], y:Seq[Option[A]]) = {
-      def unitVector(k: Int) = Seq.tabulate(rank)(i => if(i == k) fusionRing.coefficients.one else fusionRing.coefficients.zero)
-      add(for((xi, i) <- x.zipWithIndex; (yj, j) <- y.zipWithIndex) yield {
-        val z = coefficients.multiply(xi, yj)
-        fusionRing.multiply(unitVector(i), unitVector(j)).map(s => coefficients.multiply(Some(s), z))
-      })
-    }
-  }
-  
 }
