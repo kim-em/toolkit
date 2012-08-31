f39c5e2a9e48c4b8f5c997348c45a6e4e64dba7f
Scott Morrison <scott@tqft.net>
2012-08-31 13:19:20 +0200

    oops, should be on 2.10.0-M7

--- project/toolkit.scala
+++ project/toolkit.scala
@@ -45,7 +45,7 @@ object BuildSettings {
 
   val buildOrganization = "net.tqft"
   val buildVersion = "0.1.7"
-  val buildScalaVersion = "2.9.2"
+  val buildScalaVersion = "2.10.0-M7"
   val buildCrossScalaVersions = Seq("2.9.2", "2.10.0-M7")
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
