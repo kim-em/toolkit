5d50248087affb705f6cfbb18837d60e2415cf5b
Scott Morrison <scott@tqft.net>
2012-12-11 12:04:20 +1100

    starting work on fusion bimodule multiplication

--- algebra/src/main/scala/net/tqft/toolkit/algebra/fusion/FusionBimoduleMultiplications.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/fusion/FusionBimoduleMultiplications.scala
@@ -1,7 +1,7 @@
 package net.tqft.toolkit.algebra.fusion
 
 object FusionBimoduleMultiplications {
-  def apply[A](left: FusionBimodule[A], right: FusionBimodule[A], target: FusionBimodule[A]): List[FusionBimoduleMultiplication[A]] = {
+  def apply(left: FusionBimoduleWithDimensions, right: FusionBimoduleWithDimensions, target: FusionBimoduleWithDimensions): List[FusionBimoduleMultiplication] = {
     ???
   }
 }
