52c351373d6eae875247a29e37fd59b9140df315
Scott Morrison <scott@tqft.net>
2013-01-23 17:03:53 +1100

    graph stuff is still broken

--- algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/TriangleFreeGraph.scala
+++ algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/TriangleFreeGraph.scala
@@ -2,7 +2,7 @@ package net.tqft.toolkit.algebra.enumeration
 
 import net.tqft.toolkit.algebra.graphs.Graph
 
-case class TriangleFreeGraph(numberOfVertices: Int, edges: Set[Set[Int]]) extends Graph with CanonicalGeneration[TriangleFreeGraph, IndexedSeq[Int]] { tfg =>
+case class TriangleFreeGraph(numberOfVertices: Int, adjacencies: IndexedSeq[Seq[Int]]) extends Graph with CanonicalGeneration[TriangleFreeGraph, IndexedSeq[Int]] { tfg =>
   import net.tqft.toolkit.permutations.Permutations._
   import net.tqft.toolkit.algebra.graphs._
   override lazy val automorphisms = dreadnaut.automorphismGroup(this)
@@ -11,12 +11,12 @@ case class TriangleFreeGraph(numberOfVertices: Int, edges: Set[Set[Int]]) extend
       this.mark(Seq(l.k))
       ) })
 
-  case class Upper(independentVertices: Set[Int]) {
-    lazy val result = TriangleFreeGraph(numberOfVertices + 1, edges ++ independentVertices.map(Set(_, numberOfVertices)))
+  case class Upper(independentVertices: Seq[Int]) {
+    lazy val result = TriangleFreeGraph(numberOfVertices + 1, addVertex(independentVertices).adjacencies)
     def inverse = result.Lower(numberOfVertices)
   }
   case class Lower(k: Int) {
-    lazy val result = TriangleFreeGraph(numberOfVertices - 1, edges.collect({ case e if !e.contains(k) => e.map({ case v if v > k => v - 1; case v => v }) }))
+    lazy val result = TriangleFreeGraph(numberOfVertices - 1, deleteVertex(k).adjacencies)
   }
 
   override def upperObjects = new automorphisms.Action[Upper] {
@@ -25,13 +25,12 @@ case class TriangleFreeGraph(numberOfVertices: Int, edges: Set[Set[Int]]) extend
       def independentSubsetsOf(vertices: List[Int]): Iterator[List[Int]] = {
         vertices match {
           case Nil => Iterator(Nil)
-          case head :: tail => {
-            val connectedVertices = edges.filter(_.contains(head)).flatten
-            independentSubsetsOf(tail) ++ independentSubsetsOf(tail.filterNot(connectedVertices.contains)).map(head :: _)
+          case i :: t => {
+            independentSubsetsOf(t) ++ independentSubsetsOf(t.filterNot(adjacencies(i).contains)).map(i :: _)
           }
         }
       }
-      (for (s <- independentSubsetsOf(0 until numberOfVertices toList)) yield Upper(s.toSet)).toSet
+      (for (s <- independentSubsetsOf(0 until numberOfVertices toList)) yield Upper(s)).toSet
     }
     override def act(g: IndexedSeq[Int], upper: Upper) = {
       val h = g.inverse
--- algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/TriangleFreeGraphTest.scala
+++ algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/TriangleFreeGraphTest.scala
@@ -12,7 +12,7 @@ class TriangleFreeGraphTest extends FlatSpec with ShouldMatchers {
   "descendants" should "enumerate all triangle free graphs with up to 8 vertices" in {
     val n = 10
     
-    val g0 = TriangleFreeGraph(1, Set.empty)
+    val g0 = TriangleFreeGraph(1, IndexedSeq(Seq()))
     val graphs = g0.descendants(n - _.numberOfVertices).toStream
     for (g <- graphs) println(g)
     graphs.groupBy(_.numberOfVertices).mapValues(_.size).toSeq.sortBy(_._1).map(_._2) should equal(List(1, 2, 3, 7, 14, 38, 107, 410, 1897, 12172, 105071, 1262180, 20797002, 467871369).take(n))
