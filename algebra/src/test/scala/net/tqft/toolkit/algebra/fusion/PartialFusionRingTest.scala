6d3519b376e41e146c2c9e8d2787d89acc57e8b9
Scott Morrison <scott@tqft.net>
2013-01-27 23:21:32 +1100

    still buggy

--- algebra/src/test/scala/net/tqft/toolkit/algebra/fusion/PartialFusionRingTest.scala
+++ algebra/src/test/scala/net/tqft/toolkit/algebra/fusion/PartialFusionRingTest.scala
@@ -12,30 +12,51 @@ import net.tqft.toolkit.algebra.grouptheory.FiniteGroup
 @RunWith(classOf[JUnitRunner])
 class PartialFusionRingTest extends FlatSpec with ShouldMatchers {
 
-  {
-    import net.tqft.toolkit.functions.Memo._
-    implicit val toPartialFusionRing = { group: FiniteGroup[_] =>
-      FusionRings.Examples.representationsOf(group): PartialFusionRing
-    }.memo
+  import net.tqft.toolkit.functions.Memo._
+  implicit val toPartialFusionRing = { group: FiniteGroup[_] =>
+    FusionRings.Examples.representationsOf(group): PartialFusionRing
+  }.memo
 
-    "verifyAncestry" should "work on cyclic groups" in {
-      for (n <- 6 to 6) {
-        val pfr: PartialFusionRing = FiniteGroups.cyclicGroup(n)
-        println(pfr)
-        println(pfr.ring.basis)
-        pfr.verifyAncestry should be(true)
-      }
-    }
-    //    "verifyAncestry" should "work on symmetric groups" in {
-    //      for (n <- 2 to 2) {
-    //        FiniteGroups.symmetricGroup(n).verifyAncestry should be(true)
-    //      }
-    //    }
+  //    "verifyAncestry" should "work on cyclic groups" in {
+  //      for (n <- 4 to 7) {
+  //        val pfr: PartialFusionRing = FiniteGroups.cyclicGroup(n)
+  //        pfr.verifyAncestry should be(true)
+  //      }
+  //    }
 
-    //    "progenitor" should "always be A1" in {
-    //      for (n <- 4 to 7) {
-    //         FiniteGroups.cyclicGroup(n).progenitor.ring.rank should be(1)
-    //      }
-    //    }
+  val pfr: PartialFusionRing = FiniteGroups.symmetricGroup(4)
+  println(pfr)
+  println(pfr.parent.get)
+  val p2 = pfr.parent.get.parent.get
+  println(p2)
+  val p3 = pfr.parent.get.parent.get.parent.get
+  println(p3)
+  println(p3.children.exists(_.isomorphicTo_?(p2)))
+
+  val upperObjects = p3.upperObjects
+  for (o <- upperObjects.orbits.iterator; s <- o.elements.subsets(2); Seq(a, b) = s.toSeq) {
+    require(a.result.isomorphicTo_?(b.result), a + " not isomorphic to " + b)
   }
+
+  for (s <- upperObjects.orbits.subsets(2); Seq(o1, o2) = s.toSeq; u1 = o1.representative; u2 = o2.representative) {
+    require(!u1.result.isomorphicTo_?(u2.result), "orbits " + o1 + " and " + o2 + " contain isomorphic representatives " + u1 + " and " + u2)
+  }
+
+  require(p3.verifyUpperOrbits.forall(_ == true))
+  val orbit = p3.upperObjects.orbits.find(_.elements.exists(_.result.isomorphicTo_?(p2))).get
+  println(orbit.elements)
+  println(p3.upperObjects.elements.map(_.result).exists(_.isomorphicTo_?(p2)))
+
+  //        "verifyAncestry" should "work on symmetric groups" in {
+  //          for (n <- 4 to 4) {
+  //            FiniteGroups.symmetricGroup(n).verifyAncestry should be(true)
+  //          }
+  //        }
+
+  //        "progenitor" should "always be A1" in {
+  //          for (n <- 4 to 7) {
+  //             FiniteGroups.cyclicGroup(n).progenitor.ring.rank should be(1)
+  //          }
+  //        }
+
 }
