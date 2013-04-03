c294c23d244a44f9ba192043a2d1127a6a604b87
Scott Morrison <scott@tqft.net>
2013-04-03 16:51:49 +1100

    updating build

--- project/toolkit.scala
+++ project/toolkit.scala
@@ -45,8 +45,8 @@ object BuildSettings {
 
   val buildOrganization = "net.tqft"
   val buildVersion = "0.1.16-SNAPSHOT"
-  val buildScalaVersion = "2.10.0"
-  val buildCrossScalaVersions = Seq("2.9.3-RC1", "2.10.0")
+  val buildScalaVersion = "2.10.1"
+  val buildCrossScalaVersions = Seq("2.9.3", "2.10.1")
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := buildOrganization,
@@ -57,8 +57,8 @@ object BuildSettings {
     resolvers := sonatypeResolvers /* ++ SonatypeSettings.publishing */,
     libraryDependencies += {
         val (scalatestVersion, scalatestScalaVersion) = buildScalaVersion match {
-                case sv if sv.startsWith("2.9") => ("2.0.M5b", "2.9.0")
-                case sv if sv.startsWith("2.10") => ("2.0.M5b", "2.10")
+                case sv if sv.startsWith("2.9") => ("1.9.1", "2.9.0")
+                case sv if sv.startsWith("2.10") => ("1.9.1", "2.10")
         }
         ("org.scalatest" % ("scalatest_" + scalatestScalaVersion) % scalatestVersion % "test" )
     },
@@ -121,6 +121,6 @@ object Dependencies {
 	}
         val jets3t = "net.java.dev.jets3t" % "jets3t" % "0.9.0"
         val typica = "com.google.code.typica" % "typica" % "1.7.2"
-	val guava = "com.google.guava" % "guava" % "12.0"
+	val guava = "com.google.guava" % "guava" % "14.0"
 }
 
