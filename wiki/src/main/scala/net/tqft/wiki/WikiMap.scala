27cd6cc418d7df795289a2e99bdbfd284a9a8192
Scott Morrison <scott@tqft.net>
2014-01-09 01:27:25 +1100

    ruggedizing WikiMap

--- wiki/src/main/scala/net/tqft/wiki/WikiMap.scala
+++ wiki/src/main/scala/net/tqft/wiki/WikiMap.scala
@@ -5,6 +5,7 @@ import net.tqft.toolkit.Logging
 import scala.io.Source
 import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
+import net.tqft.toolkit.Throttle
 
 trait WikiMap extends scala.collection.mutable.Map[String, String] {
   def wikiScriptURL: String
@@ -12,17 +13,28 @@ trait WikiMap extends scala.collection.mutable.Map[String, String] {
   var _username: String = null
   var _password: String = null
 
+  var throttle = Throttle.linearBackoff(10000)
+
   def login(username: String, password: String) {
     _username = username
     _password = password
-    driver.get(wikiScriptURL + "?title=Special:UserLogin")
-    val name = driver.findElement(By.id("wpName1"))
-    name.clear()
-    name.sendKeys(username)
-    val pass = driver.findElement(By.id("wpPassword1"))
-    pass.clear()
-    pass.sendKeys(password)
-    driver.findElement(By.id("wpLoginAttempt")).click
+    try {
+      driver.get(wikiScriptURL + "?title=Special:UserLogin")
+      val name = driver.findElement(By.id("wpName1"))
+      name.clear()
+      name.sendKeys(username)
+      val pass = driver.findElement(By.id("wpPassword1"))
+      pass.clear()
+      pass.sendKeys(password)
+      driver.findElement(By.id("wpLoginAttempt")).click
+      throttle(true)
+    } catch {
+      case e: Exception => {
+        Logging.warn("Exception while trying to log in: ", e)
+        throttle(false)
+        login(username, password)
+      }
+    }
   }
 
   private def driver = FirefoxDriver.driverInstance
@@ -53,14 +65,15 @@ trait WikiMap extends scala.collection.mutable.Map[String, String] {
         driver.get(actionURL(kv._1, "edit"))
         driver.asInstanceOf[JavascriptExecutor].executeScript("document.getElementById('" + "wpTextbox1" + "').value = \"" + kv._2.replaceAllLiterally("\n", "\\n").replaceAllLiterally("\"", "\\\"") + "\";");
         driver.findElement(By.id("wpSave")).click
+        throttle(true)
       } catch {
         case e: Exception => {
           Logging.warn("Exception while editing wiki page: ", e)
+          throttle(false)
           driver.quit()
           if (_username != null) {
             login(_username, _password)
           }
-
           +=(kv)
         }
       }
