8624b9d2776a0489936e4d7d747d08c885da5406
Scott Morrison <scott@tqft.net>
2014-01-25 16:39:31 +1300

    wikimap can read pages via SQL

--- wiki/src/test/scala/net/tqft/wiki/WikiMapTest.scala
+++ wiki/src/test/scala/net/tqft/wiki/WikiMapTest.scala
@@ -25,6 +25,11 @@ class WikiMapTest extends FlatSpec with ShouldMatchers {
     wm("Sandbox") should equal("testbot here\n 2/2")
   }
 
+  "get" should "load a page via SQL if a jdbc string has been specified" in {
+    wm.enableSQLReads("jdbc:mysql://mysql.tqft.net/tqft_net?user=readonly1&password=foobar1", "mlp_")
+    wm("Main Page") should not be ('empty)
+  }
+  
 //  "-=" should "delete a page" in {
 //    wm.login("testbot", "zytopex")
 //    wm("User:Testbot/Sandbox") = "testbot here"
