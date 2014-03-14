f97fcf752133f656c8d0156f6192cc97a427f67e
Scott Morrison <scott@tqft.net>
2014-03-15 00:19:11 +1100

    broke the compiler?

--- algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/OdometerTest.scala
+++ algebra/src/test/scala/net/tqft/toolkit/algebra/enumeration/OdometerTest.scala
@@ -7,34 +7,31 @@ import org.junit.runner.RunWith
 import scala.math._
 
 @RunWith(classOf[JUnitRunner])
-class OdometerTest extends FlatSpec with ShouldMatchers  {
-		
+class OdometerTest extends FlatSpec with ShouldMatchers {
+
   "Odometer" should "emulate a real odometer, as in a car!" in {
-	  implicit val carLimit = { l: List[Int] => !(l.exists(_ > 9)) }
-	  import ListOdometer._
-	  Odometer(List(0,0,0)).toList.size should equal (1000)
+    val carLimit = { l: List[Int] => !(l.exists(_ > 9)) }
+    Odometer(carLimit)(List(0, 0, 0)).toList.size should equal(1000)
   }
- 
+
   "Odometer" should "find small vectors in the positive cone of a lattice" in {
-	  implicit val normList = { l: List[Int] => ((l map { pow(_,2) }) reduceLeft { _ + _ }) <= 5 }
-	  import ListOdometer._
-	  Odometer(List(0,0,0)).toList.size should equal (17)
+    val normList = { l: List[Int] => ((l map { pow(_, 2) }) reduceLeft { _ + _ }) <= 5 }
+    Odometer(normList)(List(0, 0, 0)).toList.size should equal(17)
   }
-  
+
   "Odometer" should "find matrices with small row sums" in {
-	  val sumSmall = { l: List[Int] => (l reduceLeft { _ + _ }) <= 2 }
-	  val allSumsSmall = { m: List[List[Int]] => (m map sumSmall).foldLeft(true)(_ && _) }
-	  val matrixOdometer = { matrix: List[List[Int]] => ListOdometer.odometerA(matrix)(ListOdometer.odometerInt _) }
-	  
-	  Odometer(List(List(0,0), List(0,0)))(matrixOdometer, allSumsSmall).toList.size should equal (36)
+    val sumSmall = { l: List[Int] => (l reduceLeft { _ + _ }) <= 2 }
+    val allSumsSmall = { m: List[List[Int]] => (m map sumSmall).foldLeft(true)(_ && _) }
+
+    Odometer(allSumsSmall)(List(List(0, 0), List(0, 0))).toList.size should equal(36)
   }
-  
+
   // FIXME return this to the fusionatlas package
-//  "Odometer" should "find some graphs extending Haagerup" in {
-//	implicit val limit: Bigraph => Boolean = { _.isFPEigenvaluePossiblyBelow(sqrt(5)) }
-//		
-//	val Haagerup = Bigraph("gbg1v1v1v1p1v1x0p0x1v1x0p0x1")
-//	Odometer(Bigraph(Haagerup, new RectangularMatrix(List(List(0,0))))).size should equal (4)
-//  }
+  //  "Odometer" should "find some graphs extending Haagerup" in {
+  //	implicit val limit: Bigraph => Boolean = { _.isFPEigenvaluePossiblyBelow(sqrt(5)) }
+  //		
+  //	val Haagerup = Bigraph("gbg1v1v1v1p1v1x0p0x1v1x0p0x1")
+  //	Odometer(Bigraph(Haagerup, new RectangularMatrix(List(List(0,0))))).size should equal (4)
+  //  }
 
 }
