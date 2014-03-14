fa99fc17cfb549db0756f63f686ff89ec2a641f4
Scott Morrison <scott@tqft.net>
2014-03-14 15:47:05 +1100

    a test app

---
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/principalgraphs/SubfactorClassifierApp.scala
@@ -0,0 +1,8 @@
+package net.tqft.toolkit.algebra.principalgraphs
+
+object SubfactorClassifierApp extends App {
+	val A1 = OddDepthSubfactorWeed(6.0, OddDepthPairOfBigraphsWithDuals(OddDepthBigraphWithDuals(Bigraph(1, Seq(Seq.empty)), Seq(IndexedSeq(1))), OddDepthBigraphWithDuals(Bigraph(1, Seq(Seq.empty)), Seq(IndexedSeq(1)))))
+	for(d <- A1.descendants()) {
+	  println(d)
+	}
+}
