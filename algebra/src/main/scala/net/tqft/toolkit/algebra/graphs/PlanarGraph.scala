19cd31aa7b1285f9e60b1c4b5e9b549eeeb226a9
Scott Morrison <scott@tqft.net>
2014-03-23 10:42:36 +1100

    applying reductions

--- algebra/src/main/scala/net/tqft/toolkit/algebra/graphs/PlanarGraph.scala
+++ algebra/src/main/scala/net/tqft/toolkit/algebra/graphs/PlanarGraph.scala
@@ -23,11 +23,12 @@ import net.tqft.toolkit.algebra.RationalFunction
 //  }
 //}
 
+case class RotationDefects[A](contents: A, totalRotations: Map[Int, Int])
+
 case class PlanarGraph(
   numberOfBoundaryPoints: Int,
   vertexLabels: IndexedSeq[Int],
-  vertexFlags: IndexedSeq[Seq[(Int, Int)]],
-  loops: Int = 0) {
+  vertexFlags: IndexedSeq[Seq[(Int, Int)]]) {
 
   val numberOfVertices = vertexFlags.size
   def vertices = 0 until numberOfVertices
@@ -81,6 +82,15 @@ case class PlanarGraph(
     }
     ColouredGraph(numberOfVertices, adjacencies, vertexLabels)
   }
+
+  case class Subgraphs(shape: PlanarGraph) {
+    case class Excision(cut: PlanarGraph, depth: Int, totalRotations: Map[Int, Int]) {
+      def replace(other: PlanarGraph): PlanarGraph = ???
+    }
+
+    def excisions: Iterator[Excision] = ???
+    //    def replacements(other: PlanarGraph) = excisions.map(e => RotationDefects(e.replace(other), e.totalRotations))
+  }
 }
 
 object PlanarGraph {
@@ -91,9 +101,8 @@ object PlanarGraph {
 
     val labels: IndexedSeq[Int] = IndexedSeq(-1, label)
     val flags = IndexedSeq(
-    	Seq.tabulate(k)(i => (i, i + 1 mod k)),
-    	Seq.tabulate(k)(i => (i, i))
-    )
+      Seq.tabulate(k)(i => (i, i + 1 mod k)),
+      Seq.tabulate(k)(i => (i, i)))
     PlanarGraph(k, labels, flags)
   }
 
@@ -122,7 +131,6 @@ trait Spider[A] {
 case class Disk[C](circumference: Int, contents: C)
 
 object Spider {
-
   implicit def graphSpider: Spider[PlanarGraph] = {
     new Spider[PlanarGraph] {
       override def circumference(graph: PlanarGraph) = graph.numberOfBoundaryPoints
@@ -150,9 +158,9 @@ object Spider {
       override def stitch(graph: PlanarGraph) = {
         val f0 = graph.outerFace
         val f1 = graph.vertexFlags(0)(1)._2
-        val nf = if(f0 < f1) f0 else f1
+        val nf = if (f0 < f1) f0 else f1
         def relabelFace(f: Int) = {
-          if(f == f0 || f == f1) {
+          if (f == f0 || f == f1) {
             nf
           } else {
             f
@@ -161,6 +169,7 @@ object Spider {
         def flags = ???
         PlanarGraph(graph.numberOfBoundaryPoints - 2, graph.vertexLabels, flags)
       }
+
       override def canonicalForm(graph: PlanarGraph) = {
         ???
       }
@@ -177,52 +186,109 @@ object Spider {
 
 }
 
-trait LinearSpider[R, M] extends Spider[M] with Module[R, M]
+trait LinearSpider[R, M] extends Spider[M] with Module[R, M] {
+  def ring: Ring[R]
+}
 
 object LinearSpider {
   def rationalSpider = implicitly[LinearSpider[RationalFunction[Int], Disk[Map[PlanarGraph, RationalFunction[Int]]]]]
 
-  implicit def lift[A: Spider, R: Ring]: LinearSpider[R, Map[A, R]] = {
-    val spider = implicitly[Spider[A]]
-    val ring = implicitly[Ring[R]]
-    new LinearSpider[R, Map[A, R]] {
+  class MapLinearSpider[A: Spider, R: Ring] extends LinearSpider[R, Map[A, R]] {
+    val diagramSpider = implicitly[Spider[A]]
+    override val ring = implicitly[Ring[R]]
 
-      private def mapKeys(f: A => A)(map: TraversableOnce[(A, R)]) = {
-        val newMap = scala.collection.mutable.Map[A, R]()
-        for ((a, r) <- map) {
-          val s = f(a)
-          newMap(s) = newMap.get(s).map(v => ring.add(v, r)).getOrElse(r)
-        }
-        Map() ++ newMap.filter(_._2 != ring.zero)
+    private def mapKeys(f: A => A)(map: TraversableOnce[(A, R)]) = {
+      val newMap = scala.collection.mutable.Map[A, R]()
+      for ((a, r) <- map) {
+        val s = f(a)
+        newMap(s) = newMap.get(s).map(v => ring.add(v, r)).getOrElse(r)
       }
-
-      override def rotate(map: Map[A, R], k: Int) = map.map(p => (spider.rotate(p._1, k), p._2))
-      override def tensor(map1: Map[A, R], map2: Map[A, R]) = {
-        val newMap = scala.collection.mutable.Map[A, R]()
-        for ((a, r) <- map1; (b, s) <- map2) {
-          val t = spider.tensor(a, b)
-          val p = ring.multiply(r, s)
-          newMap(t) = newMap.get(t).map(v => ring.add(v, p)).getOrElse(p)
-        }
-        Map() ++ newMap.filter(_._2 != ring.zero)
-      }
-      override def stitch(map: Map[A, R]) = mapKeys(spider.stitch)(map)
-      override def canonicalForm(map: Map[A, R]) = mapKeys(spider.canonicalForm)(map)
-      override def circumference(map: Map[A, R]) = spider.circumference(map.head._1)
-
-      override def zero = Map.empty
-      override def add(map1: Map[A, R], map2: Map[A, R]) = mapKeys(x => x)(map1.iterator ++ map2.iterator)
-      override def scalarMultiply(r: R, map: Map[A, R]) = map.mapValues(v => ring.multiply(r, v)).filter(_._2 != ring.zero)
-      override def negate(map: Map[A, R]) = map.mapValues(v => ring.negate(v))
+      Map() ++ newMap.filter(_._2 != ring.zero)
     }
+
+    override def rotate(map: Map[A, R], k: Int) = map.map(p => (diagramSpider.rotate(p._1, k), p._2))
+    override def tensor(map1: Map[A, R], map2: Map[A, R]) = {
+      val newMap = scala.collection.mutable.Map[A, R]()
+      for ((a, r) <- map1; (b, s) <- map2) {
+        val t = diagramSpider.tensor(a, b)
+        val p = ring.multiply(r, s)
+        newMap(t) = newMap.get(t).map(v => ring.add(v, p)).getOrElse(p)
+      }
+      Map() ++ newMap.filter(_._2 != ring.zero)
+    }
+    override def stitch(map: Map[A, R]) = mapKeys(diagramSpider.stitch)(map)
+    override def canonicalForm(map: Map[A, R]) = mapKeys(diagramSpider.canonicalForm)(map)
+    override def circumference(map: Map[A, R]) = diagramSpider.circumference(map.head._1)
+
+    override def zero = Map.empty
+    override def add(map1: Map[A, R], map2: Map[A, R]) = mapKeys(x => x)(map1.iterator ++ map2.iterator)
+    override def scalarMultiply(r: R, map: Map[A, R]) = map.mapValues(v => ring.multiply(r, v)).filter(_._2 != ring.zero)
+    override def negate(map: Map[A, R]) = map.mapValues(v => ring.negate(v))
   }
 
+  implicit def lift[A: Spider, R: Ring]: LinearSpider[R, Map[A, R]] = new MapLinearSpider[A, R]
+
   implicit def diskLinearSpider[A, R, M](implicit spider: LinearSpider[R, M]): LinearSpider[R, Disk[M]] = new Spider.DiskSpider(spider) with LinearSpider[R, Disk[M]] {
+    override def ring = spider.ring
     override def zero = ???
     override def add(disk1: Disk[M], disk2: Disk[M]) = Disk(disk1.circumference, spider.add(disk1.contents, disk2.contents))
     override def scalarMultiply(r: R, disk: Disk[M]) = Disk(disk.circumference, spider.scalarMultiply(r, disk.contents))
     override def negate(disk: Disk[M]) = Disk(disk.circumference, spider.negate(disk.contents))
   }
-
 }
 
+case class Reduction[A, R](big: A, small: Map[A, R])
+
+trait SubstitutionSpider[A, R] extends LinearSpider.MapLinearSpider[A, R] {
+  val eigenvalue: Int => R
+
+  def allReplacements(reduction: Reduction[A, R])(diagram: A): Iterator[Map[A, R]]
+  def replace(reduction: Reduction[A, R])(element: Map[A, R]): Map[A, R] = {
+    val newMap = scala.collection.mutable.Map[A, R]()
+    for ((a, r) <- element) {
+      import net.tqft.toolkit.collections.Iterators._
+      val m: Map[A, R] = allReplacements(reduction)(a).headOption.getOrElse(Map(a -> ring.one))
+      for ((b, t) <- m) {
+        val p = ring.multiply(r, t)
+        newMap(b) = newMap.get(b).map(v => ring.add(v, p)).getOrElse(p)
+      }
+    }
+    Map() ++ newMap.filter(_._2 != ring.zero)
+  }
+
+  def replace(reductions: Seq[Reduction[A, R]])(element: Map[A, R]): Map[A, R] = {
+    reductions.iterator.map(r => replace(r)(element)).find(_ != element).getOrElse(element)
+  }
+  def replaceRepeatedly(reductions: Seq[Reduction[A, R]])(element: Map[A, R]) = {
+    import net.tqft.toolkit.functions.FixedPoint._
+    (replace(reductions) _).fixedPoint(element)
+  }
+}
+
+object SubstitutionSpider {
+  class PlanarGraphMapSubstitutionSpider[R: Ring](override val eigenvalue: Int => R) extends LinearSpider.MapLinearSpider[PlanarGraph, R] with SubstitutionSpider[PlanarGraph, R] {
+    override def allReplacements(reduction: Reduction[PlanarGraph, R])(diagram: PlanarGraph) = {
+      for (excision <- diagram.Subgraphs(reduction.big).excisions) yield {
+        val eigenvalueFactor = ring.product(excision.totalRotations.map({
+          case (label, p) => ring.power(eigenvalue(label), p)
+        }))
+        val newMap = scala.collection.mutable.Map[PlanarGraph, R]()
+        for ((a, r) <- reduction.small) {
+          val b = diagramSpider.canonicalForm(excision.replace(a))
+          val p = ring.multiply(eigenvalueFactor, r)
+          newMap(b) = newMap.get(b).map(v => ring.add(v, p)).getOrElse(p)
+        }
+        Map() ++ newMap.filter(_._2 != ring.zero)
+      }
+    }
+  }
+
+  def withEigenvalues[R: Ring](eigenvalue: Int => R): SubstitutionSpider[PlanarGraph, R] = new PlanarGraphMapSubstitutionSpider(eigenvalue)
+}
+
+trait ReductionSpider[A, R] extends SubstitutionSpider[A, R] {
+  def reductions: Seq[Reduction[A, R]]
+  override def canonicalForm(m: Map[A, R]) = super.canonicalForm(replaceRepeatedly(reductions)(m))
+}
+
+class PlanarGraphReductionSpider[R: Ring](eigenvalue: Int => R, override val reductions: Seq[Reduction[PlanarGraph, R]]) extends SubstitutionSpider.PlanarGraphMapSubstitutionSpider[R](eigenvalue) with ReductionSpider[PlanarGraph, R]
