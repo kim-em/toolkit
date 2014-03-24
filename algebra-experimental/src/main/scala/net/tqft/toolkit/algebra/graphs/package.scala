90fb00f722ff9ae96db24d89b7599655d1566885
Scott Morrison <scott@tqft.net>
2014-03-24 23:10:18 +1100

    getting closer

--- algebra-experimental/src/main/scala/net/tqft/toolkit/algebra/graphs/package.scala
+++ algebra-experimental/src/main/scala/net/tqft/toolkit/algebra/graphs/package.scala
@@ -1,17 +1,9 @@
 package net.tqft.toolkit.algebra
 
-package object principalgraphs {
+package object graphs {
   implicit class RichSeq[A](x: Seq[A]) {
     def most = x.dropRight(1)
     def removed(i: Int) = x.take(i) ++ x.drop(i + 1)
     def secondLast = x(x.size - 2)
-  }
-
-  implicit class RichBoolean(x: Boolean) {
-    def option[A](a: => A) = if (x) {
-      Some(a)
-    } else {
-      None
-    }
   }
 }
