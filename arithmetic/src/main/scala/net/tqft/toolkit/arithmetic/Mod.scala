c9fbfd2080cb9c3902b474b2851320b878a96aa9
Scott Morrison <scott@tqft.net>
2014-03-25 19:10:17 +1100

    working on graphs

--- arithmetic/src/main/scala/net/tqft/toolkit/arithmetic/Mod.scala
+++ arithmetic/src/main/scala/net/tqft/toolkit/arithmetic/Mod.scala
@@ -3,24 +3,30 @@ package net.tqft.toolkit.arithmetic
 import scala.language.implicitConversions
 
 object Mod {
-	implicit class ModdableInt(x: Int) {
-	  def mod(k: Int) = {
-//	    require(k != 0)
-	    if(x < 0) {
-	      -((-x-1) % k) + k - 1
-	    } else {
-	      x % k
-	    }
-	  }
-	}
-	implicit class ModdableLong(x: Long) {
-	  def mod(k: Long) = {
-//	    require(k != 0)
-	    if(x < 0) {
-	      -((-x-1) % k) + k - 1
-	    } else {
-	      x % k
-	    }
-	  }
-	}
+  implicit class ModdableInt(x: Int) {
+    def mod(k: Int) = {
+      if (k == 0) {
+        x
+      } else {
+        if (x < 0) {
+          -((-x - 1) % k) + k - 1
+        } else {
+          x % k
+        }
+      }
+    }
+  }
+  implicit class ModdableLong(x: Long) {
+    def mod(k: Long) = {
+      if (k == 0) {
+        x
+      } else {
+        if (x < 0) {
+          -((-x - 1) % k) + k - 1
+        } else {
+          x % k
+        }
+      }
+    }
+  }
 }
