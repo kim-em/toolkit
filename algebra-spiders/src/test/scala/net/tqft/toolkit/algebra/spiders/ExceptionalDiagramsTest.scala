73d6751ee58e101f1ac119b2aad048103f3ca1a6
Scott Morrison <scott@tqft.net>
2014-04-13 19:34:08 -0600

    hack hack hack

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/ExceptionalDiagramsTest.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/ExceptionalDiagramsTest.scala
@@ -12,5 +12,20 @@ class ExceptionalDiagramsTest extends FlatSpec with Matchers with IsomorphismMat
   "byNumberOfVertices" should "find 1 diagrams with 0 boundary points and 0 vertices" in {
     exceptionalDiagrams.byNumberOfVertices(0, Map.empty[VertexType, Int]).size should equal(1)
   }
+  "byNumberOfVertices" should "find 2 diagrams with 4 boundary points and 0 vertices" in {
+    exceptionalDiagrams.byNumberOfVertices(4, Map.empty[VertexType, Int]).size should equal(2)
+  }
+  "byNumberOfVertices" should "find 2 diagrams with 4 boundary points and 2 vertices" in {
+    exceptionalDiagrams.byNumberOfVertices(4, Map(VertexType(3,1)->2)).size should equal(2)
+  }
+  "byNumberOfVertices" should "find 2 diagrams with 4 boundary points and 4 vertices" in {
+    exceptionalDiagrams.byNumberOfVertices(4, Map(VertexType(3,1)->4)).size should equal(1)
+  }
+  "byNumberOfVertices" should "find 2 diagrams with 4 boundary points and 1 crossing" in {
+    exceptionalDiagrams.byNumberOfVertices(4, Map(VertexType(4,2)->1)).size should equal(2)
+  }
+  "byNumberOfVertices" should "find 4 diagrams with 4 boundary points and 2 crossing" in {
+    exceptionalDiagrams.byNumberOfVertices(4, Map(VertexType(4,2)->2)).size should equal(4)
+  }
 
 }
