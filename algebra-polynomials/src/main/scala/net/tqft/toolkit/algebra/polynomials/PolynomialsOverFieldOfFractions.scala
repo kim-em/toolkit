0aea6eec8c5e20f9e09072e0b696e087f03fa2a6
Scott Morrison <scott@tqft.net>
2014-04-14 09:14:38 -0600

    renaming some files

---
+++ algebra-polynomials/src/main/scala/net/tqft/toolkit/algebra/polynomials/PolynomialsOverFieldOfFractions.scala
@@ -0,0 +1,20 @@
+package net.tqft.toolkit.algebra.polynomials
+
+import net.tqft.toolkit.algebra._
+
+abstract class PolynomialsOverFieldOfFractions[A: GCDRing] extends PolynomialsOverField[Fraction[A]] {
+  def primitivePartOverFractions(q: Polynomial[Fraction[A]]): Polynomial[A] = {
+    val gcdRing = implicitly[GCDRing[A]]
+
+    val c = gcdRing.product(q.coefficients.values.map(_.denominator))
+    val p = scalarMultiply(c, q).coefficients.mapValues(_.numerator)
+    val g = gcdRing.gcd(p.values.toSeq: _*)
+    p.mapValues(x => gcdRing.exactQuotient(x, g))
+  }
+}
+object PolynomialsOverFieldOfFractions {
+  implicit def over[A: GCDRing]: PolynomialsOverFieldOfFractions[A] = new PolynomialsOverFieldOfFractions[A] {
+    override def ring = implicitly[Field[Fraction[A]]]
+  }
+}
+
