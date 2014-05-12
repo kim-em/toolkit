c9c70b229b2d43b36b9fb1792b016bdd50344934
Scott Morrison <scott@tqft.net>
2014-05-13 07:57:01 +1000

    spiders

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/examples/SpiderData.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/examples/SpiderData.scala
@@ -10,52 +10,68 @@ import net.tqft.toolkit.algebra.mathematica._
 import net.tqft.toolkit.algebra.spiders.VertexType
 import net.tqft.toolkit.algebra.polynomials.MultivariableRationalFunction
 import net.tqft.toolkit.algebra.polynomials.MultivariablePolynomialAlgebra
+import net.tqft.toolkit.algebra.polynomials.MultivariablePolynomialAlgebras
 
 case class SpiderData(
   spider: MultivariableRationalFunctionSpider[Fraction[BigInt]],
   polyhedra: Seq[String],
   groebnerBasis: Seq[MultivariablePolynomial[Fraction[BigInt], String]],
+  nonzero: Seq[MultivariablePolynomial[Fraction[BigInt], String]],
   relations: Seq[Seq[Map[PlanarGraph, MultivariablePolynomial[Fraction[BigInt], String]]]],
   dimensionBounds: Seq[Int],
   consideredDiagramsVertexBound: Seq[Int],
   consideredDiagrams: Seq[Seq[PlanarGraph]],
-  preferredSpanningSet: Seq[Option[Seq[PlanarGraph]]],
-  preferredBasis: Seq[Option[Seq[PlanarGraph]]]) {
-  
-  def considerDiagrams(boundary: Int, vertices: Int): SpiderData = {
-    val newConsideredDiagramsVertexBound = consideredDiagramsVertexBound.padTo(boundary + 1, 0).updated(boundary, vertices)
+  independentDiagrams: Seq[Seq[PlanarGraph]]) {
+
+  lazy val quotientRing = Field.fieldOfFractions(
+    MultivariablePolynomialAlgebras.quotient(groebnerBasis))
+
+  def considerDiagram(p: PlanarGraph): Seq[SpiderData] = {
+    val boundary = p.numberOfBoundaryPoints
     val newConsideredDiagrams = {
       val padded = consideredDiagrams.padTo(boundary + 1, Seq.empty)
-      padded.updated(boundary, (padded(boundary) ++ spider.reducedDiagrams(boundary, vertices)).distinct)
+      padded.updated(boundary, (padded(boundary) :+ p).distinct)
     }
-    val diagramsToConsider = newConsideredDiagrams(boundary)
-
-    if (diagramsToConsider.size <= dimensionBounds(boundary)) {
-      // nothing to see here
-      copy(consideredDiagramsVertexBound = newConsideredDiagramsVertexBound, consideredDiagrams = newConsideredDiagrams)
-    } else {
-      // time to compute some determinants!
-      import KSubsets._
-      val subsets = diagramsToConsider.kSubsets(dimensionBounds(boundary) + 1)
-      val matrix = spider.innerProductMatrix(diagramsToConsider, diagramsToConsider)
-      val matrices = (for (subset <- KSubsets(diagramsToConsider.size, dimensionBounds(boundary) + 1)) yield {
-        val entries = subset.map(matrix).map(r => subset.map(r))
-        Matrix(dimensionBounds(boundary) + 1, entries)
-      })
-      val determinants = matrices.map(_.determinant.ensuring(_.denominator == implicitly[MultivariablePolynomialAlgebra[BigInt, String]].one).numerator)
-
+    val paddedIndependentDiagrams = independentDiagrams.padTo(boundary + 1, Seq.empty)
+    val candidateIndependentDiagrams = paddedIndependentDiagrams(boundary) :+ p
+    val matrix = spider.innerProductMatrix(candidateIndependentDiagrams, candidateIndependentDiagrams)
+    val determinant = Matrix(candidateIndependentDiagrams.size, matrix).determinant(quotientRing).ensuring(_.denominator == quotientRing.one.denominator).numerator
+    
+    val addIndependentDiagram: Option[SpiderData] = {
+      if (determinant != quotientRing.zero && candidateIndependentDiagrams.size <= dimensionBounds(boundary)) {
+        // TODO update nonzero
+        val newNonzero = {
+          import
+          val factors = determinant.factor
+          ???
+        }
+        val newIndependentDiagrams = paddedIndependentDiagrams.updated(boundary, candidateIndependentDiagrams)
+        Some(copy(nonzero = newNonzero,
+          consideredDiagrams = newConsideredDiagrams,
+          independentDiagrams = newIndependentDiagrams))
+      } else {
+        None
+      }
+    }
+    val addDeterminantToIdeal: Option[SpiderData] = {
       val newGroebnerBasis = {
         import mathematica.GroebnerBasis._
-        (groebnerBasis ++ determinants).computeGroebnerBasis
+        (groebnerBasis :+ determinant).computeGroebnerBasis
       }
-      
-      // TODO we should also look for new relations
-      
-      copy(
-          consideredDiagramsVertexBound = newConsideredDiagramsVertexBound,
-          consideredDiagrams = newConsideredDiagrams,
-          groebnerBasis = newGroebnerBasis)
+      Some(copy(
+        consideredDiagrams = newConsideredDiagrams,
+        groebnerBasis = newGroebnerBasis))
     }
+
+    addIndependentDiagram.toSeq ++ addDeterminantToIdeal
+  }
+
+  def considerDiagrams(boundary: Int, vertices: Int): Seq[SpiderData] = {
+    val newConsideredDiagramsVertexBound = consideredDiagramsVertexBound.padTo(boundary + 1, 0).updated(boundary, vertices)
+    val diagramsToConsider = spider.reducedDiagrams(boundary, vertices)
+
+    diagramsToConsider.foldLeft(Seq(this))({ (s: Seq[SpiderData], p: PlanarGraph) => s.flatMap(d => d.considerDiagram(p)) })
+      .map(_.copy(consideredDiagramsVertexBound = newConsideredDiagramsVertexBound))
   }
 }
 
@@ -64,7 +80,16 @@ object InvestigateTetravalentSpiders extends App {
     override def generators = Seq((VertexType(4, 1), ring.one))
   }
 
-  val initialData = SpiderData(freeTetravalentSpider, Seq.empty, Seq.empty, Seq.empty, dimensionBounds = Seq(1, 0, 1, 0, 3), Seq.empty, Seq.empty, Seq.empty, Seq.empty)
+  val initialData = SpiderData(
+    freeTetravalentSpider,
+    Seq.empty,
+    Seq.empty,
+    Seq.empty,
+    Seq.empty,
+    dimensionBounds = Seq(1, 0, 1, 0, 3),
+    Seq.empty,
+    Seq.empty,
+    Seq.empty)
 
-  println(initialData.considerDiagrams(0, 0).considerDiagrams(0, 2))
+  println(Seq(initialData).flatMap(_.considerDiagrams(0, 0)).flatMap(_.considerDiagrams(0, 2)))
 }
