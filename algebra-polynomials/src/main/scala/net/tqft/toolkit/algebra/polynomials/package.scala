8f76dc2fcf46f030fae30f13633775ba2e5e8370
Scott Morrison <scott@tqft.net>
2014-09-06 23:06:09 +1000

    hmm, maybe that worked

--- algebra-polynomials/src/main/scala/net/tqft/toolkit/algebra/polynomials/package.scala
+++ algebra-polynomials/src/main/scala/net/tqft/toolkit/algebra/polynomials/package.scala
@@ -5,4 +5,8 @@ package object polynomials {
   
   type RationalFunction[A] = Fraction[Polynomial[A]]
   type MultivariableRationalFunction[A, V] = Fraction[MultivariablePolynomial[A, V]]
+  
+  implicit class MultivariableRationalFunctionWrapper[A, V](f: MultivariableRationalFunction[A, V]) {
+    def variables = f.numerator.variables ++ f.denominator.variables
+  }
 }
