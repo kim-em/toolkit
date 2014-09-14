f706fcbaa22ba0e8df242c45720e41ea7b6525df
Scott Morrison <scott@tqft.net>
2014-09-14 21:34:59 +1000

    more inner products to compute

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/examples/ComputeInnerProducts.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/examples/ComputeInnerProducts.scala
@@ -8,7 +8,7 @@ import net.tqft.toolkit.algebra.polynomials.SeqPolynomial
 import net.tqft.toolkit.algebra.polynomials.MultivariablePolynomial
 
 object ComputeInnerProducts extends App {
-    
+
   lazy val `D(4,0)` = TrivalentGraphs.withoutSmallFaces.byNumberOfFaces(4, 0).toList
   lazy val `D(4,1)` = TrivalentGraphs.withoutTinyFaces.byNumberOfFaces(4, 1).toList
   def D(n: Int, k: Int) = {
@@ -23,15 +23,15 @@ object ComputeInnerProducts extends App {
 
   import mathematica.MathematicaForm._
 
-  for ((n, k) <- Seq((8,0), (8,1), (9,0), (9,1), (10,0), (11,0))) {
+  for ((n, k) <- Seq((8, 0), (8, 1), (9, 0), (9, 1), (10, 0), (11, 0), (6, 3), (7, 3), (8, 2), (9, 2), (10, 1), (11, 1))) {
     println(s"M($n, $k)")
     println(M(n, k).toMathematicaInputString)
   }
 
   implicit val polynomialForm = MathematicaForm.polynomialMathematicaForm[Fraction[BigInt]]("w")
 
-//  for ((n, k) <- Seq((4, 0), (4, 1), (5, 0), (5, 1), /*(5,6),*/ (6, 0), (6, 1), (6, 2)/*, (7, 0), (7,1), (7,2)*//*(8,0), (8,1), (9,0)*/)) {
-//    println(s"twisted M($n, $k)")
-//    println(Mw(n, k).toMathematicaInputString)
-//  }
+  //  for ((n, k) <- Seq((4, 0), (4, 1), (5, 0), (5, 1), /*(5,6),*/ (6, 0), (6, 1), (6, 2)/*, (7, 0), (7,1), (7,2)*//*(8,0), (8,1), (9,0)*/)) {
+  //    println(s"twisted M($n, $k)")
+  //    println(Mw(n, k).toMathematicaInputString)
+  //  }
 }
