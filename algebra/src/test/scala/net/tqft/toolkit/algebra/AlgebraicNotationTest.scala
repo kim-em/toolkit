07c3c140393e085ce6666ac068ba1e165599a2f9
Scott Morrison <scott@tqft.net>
2014-11-25 21:41:56 +1100

    starting to rip code out that belongs in mma

---
+++ algebra/src/test/scala/net/tqft/toolkit/algebra/AlgebraicNotationTest.scala
@@ -0,0 +1,14 @@
+package net.tqft.toolkit.algebra
+
+//import net.tqft.toolkit.algebra.polynomials.Polynomial
+
+//object AlgebraicNotationTest {
+//  val x = Polynomial(1 -> 1)
+//
+//  import AlgebraicNotation._
+//  val p = x ^ 3
+//  val q = x ^ 3 - 1
+//  val r = 2 *: x
+//  val s = (x ^ 3) + (2 *: x ^ 2) - 1
+//
+//}
