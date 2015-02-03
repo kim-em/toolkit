af804407be44422db35f7b1c535be596454a177a
Scott Morrison <scott@tqft.net>
2015-02-03 16:50:14 +1100

    fusion rings

--- algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion2/AssociativityConstraintsTest.scala
+++ algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion2/AssociativityConstraintsTest.scala
@@ -8,9 +8,18 @@ import org.scalatest._
 class AssociativityConstraintsTest extends FlatSpec with Matchers {
 
   "AssociativityConstraints.apply" should "prepare all the associativity and duality equations" in {
-    for(e <- AssociativityConstraints(2, 1).distinct) {
-      println(e)
+    for (e <- AssociativityConstraints(4,0).quadratics) {
+      println(e.current)
     }
+  }
+
+  "SystemOfQuadratics.preferredSubstitutionVariables" should "report the best variables to evaluate next" in {
+    val s = AssociativityConstraints(6, 0)
+    println(s.preferredSubstitutionVariables)
+    val s2 = s.substitute((1,1,1), 0).get
+    println(s2.preferredSubstitutionVariables)
+    val s3 = s2.substitute((1,1,2), 0).get
+    println(s3.preferredSubstitutionVariables)
   }
 
 }
