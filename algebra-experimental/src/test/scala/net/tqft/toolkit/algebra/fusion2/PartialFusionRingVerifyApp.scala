8423bf03ef7c4ca797d4142bbc68223390299568
Scott Morrison <scott@tqft.net>
2015-03-10 11:25:35 +1100

    verify tool can delete

--- algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion2/PartialFusionRingVerifyApp.scala
+++ algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion2/PartialFusionRingVerifyApp.scala
@@ -6,8 +6,6 @@ import java.io.File
 object PartialFusionRingVerifyApp extends App {
 
   val directory = new File("fusion-rings")
-
-  TreeReader.verify(directory)
-
+  TreeReader.verify(directory, args.filterNot(_.startsWith(" ")).mkString(" "), args.contains("-d"))
 
 }
