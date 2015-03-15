a6c23689ad5eabaf29db16d19ee0b378ad2ce736
Josh Chen <jaycech3n@gmail.com>
2015-03-15 14:58:09 +1300

    1. Performance tweaks; 2. Started adding functionality to handle loops/nested loops but this is lower priority for now.

--- algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/plantriHelper.scala
+++ algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/plantriHelper.scala
@@ -1,26 +1,43 @@
 package net.tqft.toolkit.algebra.spiders
 
+import java.nio.file.{Files,Paths}
+
 object plantriHelper {
+  def edgeCodeToEdgeAdj(file: String)/*: Seq[IndexedSeq[IndexedSeq[Int]]]*/ = {
+    // Takes plantri's edge code output and converts it into a Seq of 
+    // inputs for edgeAdjToPlanarGraph
+    val data = Files.readAllBytes(Paths.get(file))
+    if ( data(0) != 0 ) // See plantri-guide.txt for description of edge code. The first byte flags something.
+      data // PLACEHOLDER
+    else
+      data // PLACEHOLDER
+  }
   
-  // def convert(eAdjs: IndexedSeq[IndexedSeq[Int]]) = { // debug header
-  def convert(eAdjs: IndexedSeq[IndexedSeq[Int]]): PlanarGraph = {
+  def edgeAdjToPlanarGraph(eAdjs: IndexedSeq[IndexedSeq[Int]]): PlanarGraph = {
     // Convert plantri edge code output to internal PlanarGraph representation.
-    // Input:  the edge code of a graph G as a IndexedSeq of IndexedSeqs of integers.
-    // The IndexedSeq of integers at position v is a CW sequence of edges out of vertex v.
-    //
-    // NOTE 1: In order to correctly get the outerFace parameter for PlanarGraph we require
-    // the first vertex in the edge code to be on the outer face.
     // 
-    // NOTE 2: Doesn't work on any graph with loops!
+    // Input:  the edge adjacency list ("edge code") of a graph G as a
+    // IndexedSeq of IndexedSeqs of integers.
+    // The IndexedSeq of integers at index v is a CW sequence of edges out of vertex v.
+    //
+    // NOTES:
+    // 0. This code guaranteed safe for duals of disk triangulations output by plantri,
+    //    but is not guaranteed to work for all types of plantri output.
+    // 
+    // 1. In order to correctly get the outerFace parameter for PlanarGraph we require
+    //    the first vertex in the edge code to be on the outer face.
+    //    plantri does this by default for duals of disk triangulations.
+    // 
+    // 2. Not guaranteed safe for graphs with loops! Will implement this later if necessary.
     
     val numOfVertices = eAdjs.length
 
-    // Map edges (: Int) to their endpoints (: Set[Int])
+    // Map edges (: Int) to their endpoints (: IndexedSeq[Int])
     // OPTIMIZATION NOTE: Premature optimization is the root of all evil,
     // but I can't help thinking that using mutable.Set and going through eAdjs once for each vertex
     // and updating the corresponding edge set might be better.
     val vertexPairings = eAdjs.flatten.distinct. // get distinct edges
-      map((e: Int) => (e, (for (v <- 0 until numOfVertices if eAdjs(v).contains(e)) yield v).toSet)).
+      map((e: Int) => (e, (for (v <- 0 until numOfVertices if eAdjs(v).contains(e)) yield v))).
       toMap
 
     // Temporary variable (: IndexedSeq[(IndexedSeq[Int], Array[Int])]) en route to constructing final vertexFlags.
@@ -29,26 +46,29 @@ object plantriHelper {
     // The IndexedSeq in the first entry stores the ACW cyclic edge adjacencies, and
     // the array (mutable!) in the second entry stores the leftward face associated to each edge.
     // We will update the arrays with face labels by traversing the graph.
-    var tmpVertexFlags = eAdjs.map(L => (L.reverse, Array.fill(L.length)(0)))
+    var tmpVertexFlags = eAdjs.map(L => (L.reverse, Array.fill(L.length)(-1)))
     
     def traverse(vertex: Int, edgeIndex: Int, faceLabel: Int): Unit = {
       // Take a vertex and
       // *the index in tmpVertexFlags(startVertex)._1 of* an edge going out of it,
       // and traverse the boundary of the face to the left of startEdge,
       // updating the relevant entries in tmpVertexFlags with the face label as we go.
+      // Traversal ends when we reach a left turn that has already been traveled. 
             
       def step(currentVertex: Int, outEdge: Int): (Int, Int) = {
         // Takes the current vertex we're on, and the edge to travel down,
         // and returns a tuple of (the vertex "nextVertex" we travel to, edge "leftTurn" out of nextVertex "to the left" of outEdge)
-        val nextVertex = (vertexPairings(outEdge) - currentVertex).head
-        // Take the next edge from outEdge in the CW sequence for nextVertex 
-        val leftTurn = eAdjs(nextVertex)( (eAdjs(nextVertex).indexOf(outEdge) + 1) % eAdjs(nextVertex).length )
+        val tmpNextVertex = vertexPairings(outEdge).diff(Seq(currentVertex))
+        val nextVertex = if (tmpNextVertex.isEmpty) vertexPairings(outEdge).head else tmpNextVertex.head // Need this check to handle loops.
+                                                                                                         // (Need more to handle nested loops however.)
+        // Take the next edge from outEdge in the CW edge adjacencies for nextVertex 
+        val leftTurn = eAdjs(nextVertex)( (eAdjs(nextVertex).indexOf(outEdge) + 1) % eAdjs(nextVertex).length ) // This needs to be modified to handle loops properly.
         
         return (nextVertex, leftTurn)
       }
       
-      if ( tmpVertexFlags(vertex)._2(edgeIndex) != 0 ) // A nonzero entry indicates we've already updated the
-        Unit                                           // face label for the edge out of this vertex
+      if ( tmpVertexFlags(vertex)._2(edgeIndex) != -1 ) // If the face is not labeled -1 we've already updated the
+        Unit                                            // face label for the edge out of this vertex
       else {
         tmpVertexFlags(vertex)._2(edgeIndex) = faceLabel
         val (v,e) = step(vertex, tmpVertexFlags(vertex)._1(edgeIndex))
@@ -57,22 +77,21 @@ object plantriHelper {
     }
     
     // Traverse graph faces and update tmpVertexFlags
-    var face = 1 // face label
+    var face = 0 // face label
     for (v <- 0 until numOfVertices) {
-      var untraversedFaceAt = tmpVertexFlags(v)._2.indexOf(0) // get index of edge for which we have not yet found the left face
+      var untraversedFaceAt = tmpVertexFlags(v)._2.indexOf(-1) // get index of edge for which we have not yet found the left face
       while (untraversedFaceAt != -1) {
         traverse(v, untraversedFaceAt, face)
         face += 1
-        untraversedFaceAt = tmpVertexFlags(v)._2.indexOf(0)
+        untraversedFaceAt = tmpVertexFlags(v)._2.indexOf(-1)
       }
     }
     
     // vertexFlags: IndexedSeq[Seq[(Int, Int)]] describes the half edges coming out of each vertex
-    val vertexFlags = for (v <- 0 until numOfVertices) yield ( (for (i <- 0 until eAdjs(v).length) yield (tmpVertexFlags(v)._1(i), tmpVertexFlags(v)._2(i))).toSeq )
+    val vertexFlags = for (v <- 0 until numOfVertices) yield ( tmpVertexFlags(v)._1.zip(tmpVertexFlags(v)._2) )
     val outerFace = vertexFlags(0).head._2
     val labels = 0 until numOfVertices
-    val loops = 0 // We will not handle loops until I figure out how to best modify vertexPairings
-                  // and traverse to accommodate edges that start and end on the same vertex.  
+    val loops = 0 // vertexPairings.values.count(S => S.length == 1) once we correctly implement loop functionality
     
     return PlanarGraph(outerFace, vertexFlags, labels, loops)
   }
