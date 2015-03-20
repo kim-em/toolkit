58a20bb3627a0845584dcabdb18089354fef2a42
Scott Morrison <scott@tqft.net>
2015-03-20 15:21:30 +1100

    the test we wrote on the phone; my apologies committing stuff that doesn't compile

---
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlantriTest.scala
@@ -0,0 +1,14 @@
+package net.tqft.toolkit.algebra.spiders
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest._
+
+@RunWith(classOf[JUnitRunner])
+class PlantriTest extends FlatSpec with Matchers with IsomorphismMatchers {
+
+  "Plantri" should "count the number of graphs with cubic graphs with 6 boundary points" in {
+    Plantri.cubicGraphsWithBoundary(6).size should equal (37) 
+  }
+  
+}
