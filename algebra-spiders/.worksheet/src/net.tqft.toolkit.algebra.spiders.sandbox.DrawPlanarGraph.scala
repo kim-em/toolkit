b1197434a7dd7855eaeb25d0943fa7e061636a20
Josh Chen <jaycech3n@gmail.com>
2015-04-15 16:57:27 +1200

    Moved some things around. Committing before adding ScalaNLP dependency to project definition

--- algebra-spiders/.worksheet/src/net.tqft.toolkit.algebra.spiders.sandbox.DrawPlanarGraph.scala
+++ algebra-spiders/.worksheet/src/net.tqft.toolkit.algebra.spiders.sandbox.DrawPlanarGraph.scala
@@ -5,6 +5,6 @@ import net.tqft.toolkit.algebra.spiders._
 object DrawPlanarGraph {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(142); 
 	val G = Plantri(7,5)(2);System.out.println("""G  : net.tqft.toolkit.algebra.spiders.PlanarGraph = """ + $show(G ));$skip(135); 
                                                   
-  val vertexAdjList = for (v <- 0 until G.numberOfVertices) yield G.neighboursOf(v);System.out.println("""vertexAdjList  : scala.collection.immutable.IndexedSeq[Seq[Int]] = """ + $show(vertexAdjList ));$skip(31); val res$0 = 
-	vertexAdjList.map(println(_));System.out.println("""res0: scala.collection.immutable.IndexedSeq[Unit] = """ + $show(res$0))}
+  val vertexAdjList = for (v <- 0 until G.numberOfVertices) yield G.neighboursOf(v);System.out.println("""vertexAdjList  : scala.collection.immutable.IndexedSeq[Seq[Int]] = """ + $show(vertexAdjList ));$skip(78); 
+	for (v <- 0 until vertexAdjList.length) println(v + ": " + vertexAdjList(v))}
 }
