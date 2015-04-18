62802274823d3121c6a0eaf67ceba71bd82488de
Josh Chen <jaycech3n@gmail.com>
2015-04-18 23:12:44 +1200

    DrawPlanarGraph now draws boundary edges

--- algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/DrawPlanarGraph.scala
+++ algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/DrawPlanarGraph.scala
@@ -5,7 +5,7 @@ import scala.math._
 import breeze.linalg._
 
 object DrawPlanarGraph {
-  def apply(G: PlanarGraph, radius: Double = 2.0): String = {
+  def apply(G: PlanarGraph, radius: Double = 2.0, vertexPtSize: Double = 2, scale: Double = 1): String = {
     // Use Tutte's barycenter method to draw planar graphs.
     // Outputs LaTeX tikz code.
     
@@ -16,17 +16,30 @@ object DrawPlanarGraph {
     def round(x: Double, n: Int): Double = rint(x * pow(10, n)) / pow(10, n) // Rounds x to n decimal places, used to round coordinate values
 
     val numOfVertices = G.numberOfVertices - 1 // Remember to ignore 0 !!
-    val vertexCoords = Array.fill(numOfVertices)((0.0, 0.0))
-
     val peripheralVertices = G.neighboursOf(0).distinct // Vertices of outermost cycle; these are the ones adjacent to the boundary points
     val nonperipheralVertices = G.vertices diff peripheralVertices diff List(0) // All internal vertices
     //Debug
     //println("Peripheral vertices: " + peripheralVertices)
     //println("Nonperipheral vertices: " + nonperipheralVertices)
-
-    // First place peripheral vertices
+    
+    val vertexCoords = Array.fill(numOfVertices)((0.0, 0.0))
+    
+    val bdryPts = Array.fill(peripheralVertices.length)( Seq[(Double, Double)]( )) // ith sequence corresponds to the bdry pts adjacent to ith entry of peripheralVertices
+    
+    // Handle boundary points & outermost vertices of planar graph
     for (k <- 0 until peripheralVertices.length) {
-      vertexCoords(indexOfVertex(peripheralVertices(k))) = (round(radius * cos(2 * k * Pi / peripheralVertices.length), 6), round(radius * sin(2 * k * Pi / peripheralVertices.length), 6))
+      // First place peripheral vertices
+      val theta = 2 * k * Pi / peripheralVertices.length
+      val x = round(radius * cos(theta), 6)
+      val y = round(radius * sin(theta), 6)
+      vertexCoords(indexOfVertex(peripheralVertices(k))) = (x, y)
+      
+      // Then place boundary points adjacent to each peripheral vertex
+      val n = G.neighboursOf(peripheralVertices(k)).count(_ == 0)
+      bdryPts(k) = ( for ( k <- 1 to n )
+                             yield ( round(0.2 * radius * cos(theta + Pi/2 - k*Pi/(n+1)), 6) + x ,
+                                     round(0.2 * radius * sin(theta + Pi/2 - k*Pi/(n+1)), 6) + y)
+                          ).toSeq
     }
     //vertexCoords.map(println(_)) //Debug
 
@@ -60,20 +73,31 @@ object DrawPlanarGraph {
       //vertexCoords.map(println(_))
     }
     
-    // Write the tikz!
-    var tikzString = """\begin{tikzpicture}
-                       |[scale=1, every node/.style={circle, fill=black, inner sep=0pt, outer sep=0pt, minimum size=0pt}]""".stripMargin
-    // Place nodes
-    for (i <- 0 until vertexCoords.length) {
-      tikzString = tikzString ++ s"\n\\node (${vertexAtIndex(i)}) at ${vertexCoords(i)} {};"
-    }
-    // Draw edges. For now, just one per connected pair of vertices. Will implement multiple edges soon.
+    // Get graph edges. For now, just one per connected pair of vertices. Will implement multiple edges soon.
     var edgePairs = Seq[(Int,Int)]()
     for (pair <- G.edgeVertexIncidences.values) {
       if (pair._1 != 0 && pair._2 != 0) edgePairs = pair +: edgePairs
     }
+    
+    // Write the tikz!
+    var tikzString = s"""\\begin{tikzpicture}
+                        |[scale=$scale, every node/.style={circle, fill=black, inner sep=0pt, outer sep=0pt, minimum size=${vertexPtSize}pt}]""".stripMargin
+    // Draw boundary points
+    for (i <- 0 until peripheralVertices.length) for (j <- 0 until bdryPts(i).length) {
+      tikzString = tikzString ++ s"\n\\node (${peripheralVertices(i)}-$j) at ${bdryPts(i)(j)} {};"
+    }
+    // Draw graph vertices
+    for (i <- 0 until vertexCoords.length) {
+      tikzString = tikzString ++ s"\n\\node (${vertexAtIndex(i)}) at ${vertexCoords(i)} {};"
+    }
+    // Draw internal edges:
     val edgePairsString = edgePairs map ( (p:(Int,Int)) => "%d/%d".format(p._1,p._2) ) mkString ","
-    tikzString = tikzString ++ s"""\\foreach \\from/\\to in {${edgePairsString}}
+    // Draw boundary edges:
+    val bdryEdgesString = (for (i <- 0 until peripheralVertices.length; j <- 0 until bdryPts(i).length)
+                             yield s"${peripheralVertices(i)}/${peripheralVertices(i)}-$j"
+                           ) mkString ","
+    // Wrap up:
+    tikzString = tikzString ++ s"""\n\\foreach \\from/\\to in {$edgePairsString,$bdryEdgesString}
                                   |\\draw (\\from) -- (\\to);
                                   |\\end{tikzpicture}""".stripMargin
 
