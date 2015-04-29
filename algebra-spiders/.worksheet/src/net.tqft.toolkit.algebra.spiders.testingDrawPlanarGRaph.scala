4122469013ff405313b10eb3f2af4a9ad71fdd66
Josh Chen <jaycech3n@gmail.com>
2015-04-29 13:12:23 +1200

    removed worksheet dir

--- algebra-spiders/.worksheet/src/net.tqft.toolkit.algebra.spiders.testingDrawPlanarGRaph.scala
+++
@@ -1,5 +0,0 @@
-package net.tqft.toolkit.algebra.spiders
-
-object testingDrawPlanarGRaph {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(117); 
-  println("Welcome to the Scala worksheet")}
-}
