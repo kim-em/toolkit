823a634e33e1a60a940eae5bf7ea95b1e509c606
Scott Morrison <scott@tqft.net>
2015-06-15 11:55:25 +0200

    making pdfcrop actually do something; it needs to be explicitly told where to find gs and pdftex

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/DrawPlanarGraphApp.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/DrawPlanarGraphApp.scala
@@ -2,5 +2,5 @@ package net.tqft.toolkit.algebra.spiders
 
 object DrawPlanarGraphApp extends App {
   println(DrawPlanarGraph(PlanarGraph.polygon(6)))
-  DrawPlanarGraph.pdf("/Users/scott/scratch/hexagon.pdf", PlanarGraph.polygon(6))
+  DrawPlanarGraph.withOutputPath("/Users/scott/scratch/").writePDF(PlanarGraph.polygon(6))("hexagon.pdf")
 }
