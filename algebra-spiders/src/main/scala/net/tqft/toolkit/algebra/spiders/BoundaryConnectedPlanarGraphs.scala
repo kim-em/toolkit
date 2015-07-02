0c90c1772bccd17adfd1c0dc30ed52a7fc3762ae
Josh Chen <jaycech3n@gmail.com>
2015-07-02 15:52:56 +1200

    Merge

--- algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/BoundaryConnectedPlanarGraphs.scala
+++ algebra-spiders/src/main/scala/net/tqft/toolkit/algebra/spiders/BoundaryConnectedPlanarGraphs.scala
@@ -6,12 +6,14 @@ object BoundaryConnectedPlanarGraphs {
   def apply(n: Int, k: Int, connectedGraphGenerator: (Int, Int) => Seq[PlanarGraph]) /*Seq[PlanarGraph]*/ = {
     def product(xx: Seq[Seq[PlanarGraph]]): Seq[Seq[PlanarGraph]] =
       xx match {
-        case aa :: Nil =>
+        case aa +: Nil =>
           aa.map(Seq(_))
-        case aa :: bb :: Nil =>
+        case aa +: bb +: Nil =>
           aa.map(a => bb.map(b => Seq(a, b))).flatten
-        case aa :: bb :: cc =>
-          product(bb :: cc).map(li => aa.map(a => a +: li)).flatten
+        case aa +: bb +: cc =>
+          product(bb +: cc).map(li => aa.map(a => a +: li)).flatten
+        case Seq() +: _ =>
+          Seq()
         case _ => xx
       }
 
@@ -26,18 +28,26 @@ object BoundaryConnectedPlanarGraphs {
     val cachedConnectedGraphGenerator = Memo(connectedGraphGenerator)
 
     PlanarPartitions(n).flatMap((partition: Seq[Seq[Int]]) => {
+      //println(s"partition: $partition")
       compositions(k, partition.length).flatMap((internalFaceNumbers: Seq[Int]) => {
         assert(internalFaceNumbers.length == partition.length, "Lengths of partition and composition do not match!")
         val components = for (i <- 0 until partition.length) yield cachedConnectedGraphGenerator(partition(i).length, internalFaceNumbers(i))
+
+        //println(s"components: $components")
+        //for (x <- components) {println(x)}
+        //println(s"product(components): ${product(components)}")
+        //for (x <- product(components)) {println(x)}
+
         product(components).map((ds: Seq[PlanarGraph]) => { DiagramSpider.graphSpider.assembleAlongPlanarPartition(partition, ds) })
       })
-    })
+    }).distinct
 
   }
 
   def trivalent(n: Int, k: Int) = {
     def generator(r: Int, s: Int) = if (r > 2)
-      ConnectedTrivalentPlanarGraphs(r, s).flatMap((G: PlanarGraph) => Seq.tabulate(r)((rotation: Int) => DiagramSpider.graphSpider.rotate(G, rotation).canonicalFormWithDefect._1)).distinct
+      ConnectedTrivalentPlanarGraphs(r, s).filterNot(_.hasTinyFace).flatMap(
+        (G: PlanarGraph) => Seq.tabulate(r)((rotation: Int) => DiagramSpider.graphSpider.rotate(G, rotation).canonicalFormWithDefect._1)).distinct
     else Seq(PlanarGraph.strand)
 
     apply(n, k, generator(_, _))
