740f6db92e8c52f3ba062ddab1a7798a439dfcb3
Scott Morrison <scott@tqft.net>
2015-08-16 16:56:09 +1000

    hmm, the representation of tangles is not quite right

--- algebra-experimental/src/main/scala/net/tqft/toolkit/algebra/khovanov/Tangle.scala
+++ algebra-experimental/src/main/scala/net/tqft/toolkit/algebra/khovanov/Tangle.scala
@@ -2,26 +2,70 @@ package net.tqft.toolkit.algebra.khovanov
 
 import scala.language.implicitConversions
 
-case class Crossing(sign: Int, strands: (Int, Int, Int, Int))
+case class Crossing(label: Int, sign: Int, strands: (Int, Int, Int, Int))
 case class Arc(labels: Int*)
-case class Tangle(crossings: Seq[Crossing], arcs: Seq[Arc])
-case class Cobordism(source: Tangle, target: Tangle, frames: Seq[ElementaryCobordism])
-
-sealed trait ElementaryCobordism {
-  def source: Tangle = ??? // TODO remove the implementation here
-  def target: Tangle = ??? // TODO remove the implementation here
+trait Loop {
+  def labels: Seq[Int]
 }
-case class CreateCircle(arc: Int) extends ElementaryCobordism
-case class DestroyCircle(arc: Int) extends ElementaryCobordism
-case class Saddle(se: Int, ne: Int, nw: Int, sw: Int) extends ElementaryCobordism
-case class Twist(incoming: Int, loop: Int, outgoing: Int) extends ElementaryCobordism
-case class Untwist(incoming: Int, loop: Int, outgoing: Int) extends ElementaryCobordism
-case class R2() extends ElementaryCobordism
-case class R3() extends ElementaryCobordism
+object Loop {
+  def apply(labels: Int*) = {
+    val labels_ = labels
+    val p = labels.indexOf(labels.min)
+    new Loop {
+      override val labels = labels_.drop(p) ++ labels_.take(p)
+    }
+  }
+}
+case class Tangle(crossings: Seq[Crossing], arcs: Seq[Arc] = Seq.empty, loops: Seq[Loop] = Seq.empty)
+case class Cobordism(source: Tangle, target: Tangle, frames: Seq[ElementaryCobordism]) {
+  // convenience methods for building Cobordisms --- these all append a frame, and can look at the current target to fill in the details
+  def createLoop(loop: Loop): Cobordism = ???
+  def destroyLoop(loop: Loop): Cobordism = ???
+  def destroyLoop(label: Int): Cobordism = ???
+  def untwist(labelOnLoop: Int): Cobordism = ???
+  // ...  
+}
+
+// ugh --- there are also just global renames, e.g. to achieve a 1-click rotation of a trefoil
+// ugh --- there are also inserting and deleting labels
+sealed trait ElementaryCobordism {
+  def source: Tangle
+  def target: Tangle
+}
+case class CreateLoop(loop: Loop) extends ElementaryCobordism {
+  override def source = Tangle(Seq.empty)
+  override def target = Tangle(Seq.empty, Seq.empty, Seq(loop))
+}
+case class DestroyLoop(loop: Loop) extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
+case class Saddle(se: Int, ne: Int, nw: Int, sw: Int) extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
+
+// TODO these aren't quite right, from here on:
+case class Twist(incoming: Int, loop: Int, outgoing: Int) extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
+case class Untwist(incoming: Int, loop: Int, outgoing: Int) extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
+case class R2() extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
+case class R3() extends ElementaryCobordism {
+  override def source = ???
+  override def target = ???
+}
 
 object MovieMoves {
-  def all = MM1 #:: MM2 #:: MM3 #:: MM4 #:: MM5 #:: MM6 #:: MM7 #:: MM8 #:: MM9 #:: MM10 #:: MM11 #:: MM12 #:: MM13 #:: MM14 #:: MM15 #:: Stream.empty[(Cobordism, Cobordism)]
   // for now, let's just pick a representative of each one, and not worry about all the variants
+  def all = MM1 #:: MM2 #:: MM3 #:: MM4 #:: MM5 #:: MM6 #:: MM7 #:: MM8 #:: MM9 #:: MM10 #:: MM11 #:: MM12 #:: MM13 #:: MM14 #:: MM15 #:: Stream.empty[(Cobordism, Cobordism)]
   lazy val MM1 = {
     val source = Tangle(Seq.empty, Seq(Arc(1, 2)))
     val target = source
@@ -50,7 +94,7 @@ object Cobordism {
 
   implicit object Cobordisms extends TensorCategory[Tangle, Cobordism] {
     def compose(x: Cobordism, y: Cobordism): Cobordism = {
-      require(x.target == y.source) // perhaps we should make this more general, like we do for cans?
+      require(x.target == y.source) // perhaps we should make this more general, like we do for cans? we'd have to write something for finding complements of tangles
       Cobordism(x.source, y.target, x.frames ++ y.frames)
     }
     def identity(o: Tangle): Cobordism = Cobordism(o, o, Nil)
@@ -62,6 +106,7 @@ object Cobordism {
   }
 }
 
+// just a reminder for later
 case class AnnularTangle(cut: Seq[Int], tangle: Tangle)
 case class AnnularCobordism()
 
