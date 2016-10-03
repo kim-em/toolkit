8938af848dc8f57bba76a0d79d3fac4ae3bc3c3d
Scott Morrison <scott@tqft.net>
2016-10-03 12:04:24 +1100

    trying to use an old fusion solver

--- algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion/FusionRingsTest.scala
+++ algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/fusion/FusionRingsTest.scala
@@ -11,10 +11,12 @@ class FusionRingsTest extends FlatSpec with Matchers {
 
   "withObject" should "correctly find all fusion rings with a given object" in {
     FusionRings.withObject(FusionRings.Examples.AH1.structureCoefficients(1) /*, Some(FusionRings.Examples.AH1)*/ ).toSeq should have size (1)
+    FusionRings.withObject(Seq(Seq(0, 1, 0, 0), Seq(1, 0, 1, 1), Seq(0, 1, 0, 0), Seq(0, 1, 0, 0))).toSeq should have size (1)
+    FusionRings.withObject(Seq(Seq(0, 1, 0, 0, 0), Seq(1, 0, 1, 1, 1), Seq(0, 1, 0, 0, 0), Seq(0, 1, 0, 0, 0))).toSeq should have size (2)
   }
 
-  "representationOf" should "generate fusion rings from finite groups" in {
-    FusionRings.Examples.representationsOf(FiniteGroups.cyclicGroup(6)).globalDimensionLowerBound < 6.0 should be(true)
-    FusionRings.Examples.representationsOf(FiniteGroups.Mathieu11).globalDimensionLowerBound < FiniteGroups.Mathieu11.size should be(true)
-  }
+//  "representationOf" should "generate fusion rings from finite groups" in {
+//    FusionRings.Examples.representationsOf(FiniteGroups.cyclicGroup(6)).globalDimensionLowerBound < 6.0 should be(true)
+//    FusionRings.Examples.representationsOf(FiniteGroups.Mathieu11).globalDimensionLowerBound < FiniteGroups.Mathieu11.size should be(true)
+//  }
 }
