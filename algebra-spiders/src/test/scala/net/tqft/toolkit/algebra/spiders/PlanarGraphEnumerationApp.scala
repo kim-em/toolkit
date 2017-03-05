f9d0f8d3983adf31e6a9d1e6d3bc14fc1ef548dd
Emily Peters <emilyepeters@gmail.com>
2017-03-06 10:23:44 +1100

    working on planar graph enumeration

---
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlanarGraphEnumerationApp.scala
@@ -0,0 +1,9 @@
+package net.tqft.toolkit.algebra.spiders
+
+object PlanarGraphEnumerationApp extends App {
+  val context = PlanarGraphEnumerationContext(Seq(VertexType(3,1,1)))
+  val root = context.PlanarGraphEnumeration(PlanarGraph.polygon(5))
+  for(child<-root.children) {
+    DrawPlanarGraph.showPDF(child.G)
+  }
+}
