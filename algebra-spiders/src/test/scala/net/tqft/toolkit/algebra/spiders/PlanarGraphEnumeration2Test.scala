43353a0b9d562d1e4afd99916dd58314d75c79d1
Scott Morrison <scott@tqft.net>
2017-09-18 20:41:47 +1000

    identified, but haven't fixed, the problem

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlanarGraphEnumeration2Test.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlanarGraphEnumeration2Test.scala
@@ -17,17 +17,41 @@ class PlanarGraphEnumeration2Test extends FlatSpec with Matchers with Isomorphis
   val context = PlanarGraphEnumerationContext2(Seq(VertexType(3, 0, 1)), Seq.empty, None, None)
 
   "graphs" should "be children of their parent" in {
-    context.verify_child_of_parent(PlanarGraph.polygon(2)) should be(true)
-    context.verify_child_of_parent(PlanarGraph.polygon(3)) should be(true)
-    context.verify_child_of_parent(PlanarGraph.polygon(4)) should be(true)
-    context.verify_child_of_parent(PlanarGraph.polygon(5)) should be(true)
-
-    context.verify_child_of_parent(PlanarGraph.pentafork) should be(true)
-    context.verify_child_of_parent(PlanarGraph.hexafork) should be(true)
-    context.verify_child_of_parent(PlanarGraph.twoSquares) should be(true)
-    context.verify_child_of_parent(PlanarGraph.pentaSquare) should be(true)
-    for (i <- 0 until 3) {
-      context.verify_ancestry(spider.rotate(PlanarGraph.pentapent, i)) should be(true)
-    }
+//    context.verify_child_of_parent(PlanarGraph.polygon(2)) should be(true)
+//    context.verify_child_of_parent(PlanarGraph.polygon(3)) should be(true)
+//    context.verify_child_of_parent(PlanarGraph.polygon(4)) should be(true)
+//    context.verify_child_of_parent(PlanarGraph.polygon(5)) should be(true)
+//
+//    for (i <- 0 until 6) {
+//      context.verify_ancestry(spider.rotate(PlanarGraph.pentafork, i)) should be(true)
+//    }
+//    for (i <- 0 until 7) {
+//      context.verify_ancestry(spider.rotate(PlanarGraph.hexafork, i)) should be(true)
+//    }
+//    for (i <- 0 until 2) {
+//      context.verify_ancestry(spider.rotate(PlanarGraph.twoSquares, i)) should be(true)
+//    }
+    
+    val p = spider.rotate(PlanarGraph.pentaSquare, 2)
+    val p1 =  context.parent(p)
+    dpg.showPDF(p1)
+    val p2 =  context.parent(context.parent(p))
+    dpg.showPDF(p2)
+//    val p3 =  context.parent(context.parent(context.parent(p)))
+//    dpg.showPDF(p3)
+//    val p4 =  context.parent(context.parent(context.parent(context.parent(p))))
+//    dpg.showPDF(p4)
+//    val p5 =  context.parent(context.parent(context.parent(context.parent(context.parent(p)))))
+//    dpg.showPDF(p5)
+    
+//    context.verify_ancestry(context.parent(context.parent(context.parent(context.parent(context.parent(context.parent(p))))))) should be (true)
+//    context.verify_ancestry(p) should be(true)
+//    
+//    for (i <- 0 until 5) {
+//      context.verify_ancestry(spider.rotate(PlanarGraph.pentaSquare, i)) should be(true)
+//    }
+//    for (i <- 0 until 3) {
+//      context.verify_ancestry(spider.rotate(PlanarGraph.pentapent, i)) should be(true)
+//    }
   }
 }
