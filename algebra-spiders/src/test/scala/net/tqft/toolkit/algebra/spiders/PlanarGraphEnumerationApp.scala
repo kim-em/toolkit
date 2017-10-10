cf2c9085ddb061828818fbb77590fa0083669049
Emily Peters <emilyepeters@gmail.com>
2017-10-10 15:54:10 -0500

    mma can now enumerate graphs w/ n boundary points and k faces

--- algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlanarGraphEnumerationApp.scala
+++ algebra-spiders/src/test/scala/net/tqft/toolkit/algebra/spiders/PlanarGraphEnumerationApp.scala
@@ -1,23 +1,34 @@
 package net.tqft.toolkit.algebra.spiders
 
 object PlanarGraphEnumerationApp extends App {
-  val context = PlanarGraphEnumerationContext(Seq(VertexType(3,1,1)))
-  val root = context.PlanarGraphEnumeration(PlanarGraph.polygon(3))
-  println(root.G)
-  println("lower objects")
-  for(l <- root.lowerObjects.elements) println(l)
-  println("preferred lower object")
-  println(root.lowerObjects.allOrbits.min(root.ordering).representative)
-  
-  //  val context = PlanarGraphEnumerationContext(Seq(VertexType(3,1,1)))
-//  val root = context.PlanarGraphEnumeration(PlanarGraph.polygon(5))
-//  for(child<-root.children) {
-//    DrawPlanarGraph.showPDF(child.G)
-//  }
-//  val context = PlanarGraphEnumerationContext(Seq(VertexType(3,1,1)))
-//  val root = context.PlanarGraphEnumeration(PlanarGraph.star(3,1,1))
-//  for(child<-root.children) {
-//    DrawPlanarGraph.showPDF(child.G)
-//  }
-}
+  val dpg = {
+    System.getProperty("user.name") match {
+      case "scott"       => DrawPlanarGraph.withOutputPath("/Users/scott/scratch/graphs")
+      case "emilypeters" => DrawPlanarGraph.withOutputPath("/Users/emilypeters/Documents/scratch/graphs")
+    }
+  }
 
+  val spider = implicitly[DiagramSpider[PlanarGraph]]
+
+  {
+    val maxFaces = 2
+    val maxBoundaryPoints = 10 // we've tested up to 10
+    val root = PlanarGraph.star(4)
+    val bigon = spider.multiply(root, root, 2)
+    val triangle = spider.multiply(spider.rotate(spider.multiply(root, root, 1), 2), root, 2)
+    val context = PlanarGraphEnumerationContext(Seq(VertexType(4, 0, 1)), Seq(bigon, triangle), Seq(root), maxFaces, maxBoundaryPoints)
+
+    val descendants = context.enumerate.toStream
+
+    val maxVertices = descendants.map(_.numberOfInternalVertices).max
+    
+    val counts = for (b <- 4 to maxBoundaryPoints by 2) yield {
+      for (v <- 1 to maxVertices) yield {
+        descendants.count(g => g.numberOfBoundaryPoints == b && g.numberOfInternalVertices == v)
+      }
+    }
+
+    for (line <- counts) println(line)
+  }
+
+}
