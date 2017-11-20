001bfc9b9bd5275ff8eb08216f11201a1fb6d58c
Scott Morrison <scott@tqft.net>
2017-11-20 23:10:51 +1100

    .

--- algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/mtc/ModularDataAutomorphism.scala
+++ algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/mtc/ModularDataAutomorphism.scala
@@ -11,7 +11,7 @@ object ModularDataAutomorphism extends App {
   }
 
   for (n <- 27 to 48) {
-    for (j <- 1 to 1000; dir = new File("../modular-data/code/Modular_Data/" + n + "/" + j); if dir.exists) {
+    for (j <- 3 to 1000; dir = new File("../modular-data/code/Modular_Data/" + n + "/" + j); if dir.exists) {
       for (k <- 1 to 1000; file = new File("../modular-data/code/Modular_Data/" + n + "/" + j + "/" + k + ".txt"); if file.exists) {
         println(n + " " + j + " " + k)
         println(automorphismGroup(file).size + " = " + Factor(automorphismGroup(file).size))
