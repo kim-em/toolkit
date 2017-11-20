6bed56ec8a3613eb1e64791d4f8952b1962f8777
Scott Morrison <scott@tqft.net>
2017-11-20 23:00:01 +1100

    .

--- algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/mtc/ModularDataAutomorphism.scala
+++ algebra-experimental/src/test/scala/net/tqft/toolkit/algebra/mtc/ModularDataAutomorphism.scala
@@ -9,8 +9,8 @@ object ModularDataAutomorphism extends App {
     Dreadnaut.automorphismGroup(MatricesUpToSimultaneousRowAndColumnPermutations.convertToGraph(ModularDataLoader.superimposedST(file)))
   }
 
-  for (n <- 2 to 48) {
-    for (j <- 1 to 1000; dir = new File("../modular-data/code/Modular_Data/" + n + "/" + j); if dir.exists) {
+  for (n <- 24 to 48) {
+    for (j <- 12 to 1000; dir = new File("../modular-data/code/Modular_Data/" + n + "/" + j); if dir.exists) {
       for (k <- 1 to 1000; file = new File("../modular-data/code/Modular_Data/" + n + "/" + j + "/" + k + ".txt"); if file.exists) {
         println(n + " " + j + " " + k)
         println(automorphismGroup(file).size)
